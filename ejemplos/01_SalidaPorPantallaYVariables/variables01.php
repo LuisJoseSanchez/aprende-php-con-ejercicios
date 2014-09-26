782ae2126326e199c68e29e13ffc8fd033214eec
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-09-26 09:11:57 +0200

    Subiendo ejemplos del capítulo 1

---
+++ ejemplos/01_SalidaPorPantallaYVariables/variables01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+      $x = 24;
+      $pi = 3.1416;
+      $animal = "conejo";
+      $saludo = "hola caracola";
+      echo $x, "<br>", $pi, "<br>", $animal, "<br>", $saludo;
+    ?>
+  </body>
+</html>
