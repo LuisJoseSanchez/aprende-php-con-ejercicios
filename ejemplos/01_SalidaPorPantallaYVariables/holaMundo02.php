eede8bf903e6865a63691d57541848de80a4642d
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-09-26 09:07:17 +0200

    Subiendo ejemplos del capítulo 1

---
+++ ejemplos/01_SalidaPorPantallaYVariables/holaMundo02.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <!-- Utilizo PHP para poner en negrita una palabra -->
+    Hola
+    <?php echo "<b><u>"; ?>
+    mundo
+    <?php echo "</u></b>"; ?>
+  </body>
+</html>
---
+++ ejemplos/01_SalidaPorPantallaYVariables/holaMundo03.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <!-- Muestra una frase con HTML -->
+    Hola mundo<br>
+    <!-- Muestra una frase con PHP -->
+    <?php echo "Es muy fácil programar en PHP."; ?>
+  </body>
+</html>
