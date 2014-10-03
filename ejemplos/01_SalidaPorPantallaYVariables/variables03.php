189f10e69a813e17ae0c0e486a543fe4e2e412e1
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-03 10:10:44 +0200

    Añadido ejemplo en el capítulo 1

---
+++ ejemplos/01_SalidaPorPantallaYVariables/variables03.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+      $numero = 20;
+      $palabra = "hola";
+      print_r(get_defined_vars());
+    ?>
+  </body>
+</html>
