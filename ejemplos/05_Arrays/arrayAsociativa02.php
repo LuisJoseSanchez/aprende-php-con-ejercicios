2a81f2ce4afa2e22fd6c035cc03470707829f4e4
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-09 11:15:15 +0200

    Subiendo ejemplos del capítulo 5

---
+++ ejemplos/05_Arrays/arrayAsociativa02.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+      $estatura['Rosa'] = 168;
+      $estatura['Ignacio'] = 175;
+      $estatura['Daniel'] = 172;
+      $estatura['Rubén'] = 182;
+      echo "La estatura de Daniel es ", $estatura['Daniel'] , " cm"; 
+    ?>
+  </body>
+</html>
