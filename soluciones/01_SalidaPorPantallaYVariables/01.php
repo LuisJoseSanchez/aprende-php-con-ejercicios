01bef23c43394eb149c2ca2f42693ff3413211e3
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-31 09:14:39 +0100

    Revisando soluciones del capítulo 1.

--- soluciones/01_SalidaPorPantallaYVariables/01.php
+++ soluciones/01_SalidaPorPantallaYVariables/01.php
@@ -1,40 +1,40 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-	<div id="container">
-	    <div id="header">
-		    <h1>
-			    APRENDE PHP CON EJERCICIOS
-		    </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
-		    <h2>
-                        <br>1. Salida por pantalla y variables
-		    </h2>
-	    </div>
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
+      <div id="header">
+        <h1>
+          APRENDE PHP CON EJERCICIOS
+        </h1>
+        <h2>
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>1. Salida por pantalla y variables
+        </h2>
+      </div>
 
-	    <div id="content">
-                <?php
-                    echo "Luis José Sánchez González";
-                ?>
-                
-                <br><br><hr><br>Código fuente:<br>
-                <?php
-    highlight_string('
-    <?php
-        echo "Luis José Sánchez González";
-    ?>
-    ');
-                ?>
-	    </div>
-	    <div id="footer">
-		    © Luis José Sánchez González
-	    </div>
-	</div>
-    </body>
+      <div id="content">
+        <?php
+            echo "Luis José Sánchez González";
+        ?>
+
+        <br><br><hr><br>Código fuente:<br>
+        <?php
+          highlight_string('
+          <?php
+          echo "Luis José Sánchez González";
+          ?>
+          ');
+        ?>
+      </div>
+      <div id="footer">
+        © Luis José Sánchez González
+      </div>
+    </div>
+  </body>
 </html>
