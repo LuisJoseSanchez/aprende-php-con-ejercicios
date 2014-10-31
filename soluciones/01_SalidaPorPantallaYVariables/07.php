37ce36c0424fd6c4738b7e7727b2c09f2d38bad1
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-31 09:45:22 +0100

    Revisando soluciones del capítulo 1.

--- soluciones/01_SalidaPorPantallaYVariables/07.php
+++ soluciones/01_SalidaPorPantallaYVariables/07.php
@@ -1,56 +1,52 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>1. Salida por pantalla y variables
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>1. Salida por pantalla y variables
         </h2>
       </div>
 
       <div id="content">
-                <?php
-                    $nombre = "Luis José Sánchez González";
-        $direccion = "C/ Larios, 888 - Attic D";
-        $telefono = "951 123456"; 
-        
-        echo $nombre, "<br>";
-        echo $direccion, "<br>";
-        echo $telefono, "<br>";
-                ?>
-                
-                <br><br><hr><br>Código fuente:<br>
-                <?php
-    highlight_string('
-    <?php
+      <?php
         $nombre = "Luis José Sánchez González";
-        $direccion = "C/ Larios, 888 - Attic D";
-        $telefono = "951 123456";
+        $direccion = "C/ Larios, 888 - Ático D";
+        $telefono = "951 123456"; 
 
         echo $nombre, "<br>";
         echo $direccion, "<br>";
         echo $telefono, "<br>";
-    ?>
-    ');
-                ?>
+      ?>
+                
+      <br><br><hr><br>Código fuente:<br>
+      <?php
+        highlight_string('
+          <?php
+            $nombre = "Luis José Sánchez González";
+            $direccion = "C/ Larios, 888 - Ático D";
+            $telefono = "951 123456";
+
+            echo $nombre, "<br>";
+            echo $direccion, "<br>";
+            echo $telefono, "<br>";
+          ?>
+        ');
+      ?>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
