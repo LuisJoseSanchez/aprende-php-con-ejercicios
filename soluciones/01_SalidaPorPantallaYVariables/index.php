a1ad9a965e24e2f76c9198acf7c58dc12380d5b9
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-31 08:55:12 +0100

    Revisando soluciones del capítulo 1. Revisada la hoja de estilos y el índice

--- soluciones/01_SalidaPorPantallaYVariables/index.php
+++ soluciones/01_SalidaPorPantallaYVariables/index.php
@@ -1,96 +1,104 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-	<div id="container">
-	    <div id="header">
-		    <h1>
-			    APRENDE PHP CON EJERCICIOS
-		    </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
-		    <h2>
-                        <br>1. Salida por pantalla y variables
-		    </h2>
-	    </div>
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="container">
+      <div id="header">
+        <h1>
+          APRENDE PHP CON EJERCICIOS
+        </h1>
+        <h2>
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>1. Salida por pantalla y variables
+        </h2>
+      </div>
 
-	    <div id="content">
-		<ol>
-		    <li>
-			Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP.
-			<br /><a href="01.php">&gt;&gt; Solución</a>
-		    </li>
+      <div id="content">
+      <ol>
+        <li>
+          Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP.
+          <br /><a href="01.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>
-			Modifica el programa anterior para que muestre tu dirección y tu número de teléfono. Cada dato
-		    se debe mostrar en una línea diferente. Mezcla de alguna forma las salidas por pantalla, utilizando
-		    tanto HTML como PHP.
-			<br /><a href="02.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Modifica el programa anterior para que muestre tu dirección y tu número de teléfono. Cada dato
+          se debe mostrar en una línea diferente. Mezcla de alguna forma las salidas por pantalla, utilizando
+          tanto HTML como PHP.
+          <br /><a href="02.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su correspondiente
-		    traducción al castellano. Las palabras deben estar distribuidas en dos columnas. Utiliza la etiqueta
-		    "table" de HTML.
-		     <br /><a href="03.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su correspondiente
+          traducción al castellano. Las palabras deben estar distribuidas en dos columnas. Utiliza la etiqueta
+          "table" de HTML.
+          <br /><a href="03.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Escribe un programa que muestre tu horario de clase mediante una tabla. Aunque se puede hacer
-		    íntegramente en HTML (igual que los ejercicios anteriores), ve intercalando código HTML y PHP
-		    para familiarizarte con éste último.
-		    <br /><a href="04.php">&gt;&gt; Solución</a>
+        <li>
+          Escribe un programa que muestre tu horario de clase mediante una tabla. Aunque se puede hacer
+          íntegramente en HTML (igual que los ejercicios anteriores), ve intercalando código HTML y PHP
+          para familiarizarte con éste último.
+          <br /><a href="04.php">&gt;&gt; Solución</a>
+        </li>
 
-		    </li>
+        <li>
+          Escribe un programa que utilice las variables $x y $y. Asignales los valores 144 y 999 respectivamente.
+          A continuación, muestra por pantalla el valor de cada variable, la suma, la resta, la división
+          y la multiplicación.
+          <br /><a href="05.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Escribe un programa que utilice las variables $x y $y. Asignales los valores 144 y 999 respectivamente.
-		    A continuación, muestra por pantalla el valor de cada variable, la suma, la resta, la división
-		    y la multiplicación.
-		    <br /><a href="05.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Crea la variable $nombre y asígnale tu nombre completo. Muestra su valor por pantalla de tal
+          forma que el resultado sea el mismo que el del ejercicio 1.
+          <br /><a href="06.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>
-			Crea la variable $nombre y asígnale tu nombre completo. Muestra su valor por pantalla de tal
-		    forma que el resultado sea el mismo que el del ejercicio 1.
-		    <br /><a href="06.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Crea las variables $nombre, $direccion y $telefono y asígnales los valores adecuados. Muestra los
+          valores por pantalla de tal forma que el resultado sea el mismo que el del ejercicio 2.
+          <br /><a href="07.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Crea las variables $nombre, $direccion y $telefono y asígnales los valores adecuados. Muestra los
-		    valores por pantalla de tal forma que el resultado sea el mismo que el del ejercicio 2.
-		    <br /><a href="07.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere convertir deberá estar
+          almacenada en una variable.
+          <br /><a href="08.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere convertir deberá estar
-		    almacenada en una variable.
-		    <br /><a href="08.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Realiza un conversor de pesetas a euros. La cantidad en pesetas que se quiere convertir deberá
+          estar almacenada en una variable.
+          <br /><a href="09.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Realiza un conversor de pesetas a euros. La cantidad en pesetas que se quiere convertir deberá
-		    estar almacenada en una variable.
-		    <br /><a href="09.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Escribe un programa que pinte por pantalla una pirámide rellena a base de asteriscos. La base de
+          la pirámide debe estar formada por 9 asteriscos.
+          <br /><a href="10.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Escribe un programa que pinte por pantalla una pirámide rellena a base de asteriscos. La base de
-		    la pirámide debe estar formada por 9 asteriscos.
-		    <br /><a href="10.php">&gt;&gt; Solución</a>
-		    </li>
+        <li>
+          Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe ver únicamente el
+          contorno hecho con asteriscos).
+          <br /><a href="11.php">&gt;&gt; Solución</a>
+        </li>
 
-		    <li>Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe ver únicamente el
-		    contorno hecho con asteriscos).
-		    <br /><a href="11.php">&gt;&gt; Solución</a>
-		    </li>
-
-		    <li>Igual que el programa anterior, pero esta vez la pirámide debe aparecer invertida, con el vértice
-		    hacia abajo.
-		    <br /><a href="12.php">&gt;&gt; Solución</a>
-		    </li>
-		</ol>
-	    </div>
-	    <div id="footer">
-		    © Luis José Sánchez González
-	    </div>
-	</div>
-    </body>
+        <li>
+          Igual que el programa anterior, pero esta vez la pirámide debe aparecer invertida, con el vértice
+          hacia abajo.
+          <br /><a href="12.php">&gt;&gt; Solución</a>
+        </li>
+      </ol>
+      </div>
+      <div id="footer">
+        © Luis José Sánchez González
+      </div>
+    </div>
+  </body>
 </html>
