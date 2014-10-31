7f294bb8ac25bda9c7a0e68e3be8650b7fac95af
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-31 09:26:57 +0100

    Revisando soluciones del capítulo 1.

--- soluciones/01_SalidaPorPantallaYVariables/03.php
+++ soluciones/01_SalidaPorPantallaYVariables/03.php
@@ -1,50 +1,26 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>1. Salida por pantalla y variables
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>1. Salida por pantalla y variables
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        echo "<table>";
-    ?>
-    <tr><th>Spanish</th><th>English</th></tr>
-    <tr><td>conocimiento</td><td>knowledge</td></tr>
-    <tr><td>amor</td><td>love</td></tr>
-    <tr><td>felicidad</td><td>happiness</td></tr>
-    <tr><td>agua</td><td>water</td></tr>
-    <tr><td>aire</td><td>air</td></tr>
-    <tr><td>pastel</td><td>cake</td></tr>
-    <tr><td>dinero</td><td>money</td></tr>
-    <tr><td>libro</td><td>book</td></tr>
-    <tr><td>bosque</td><td>forest</td></tr>  
-    <?php
-        echo "</table>";
-    ?>
-                
-                <br><br><hr><br>Código fuente:<br>
-                <?php
-        highlight_string('
         <?php
-      echo "<table>";
+          echo "<table>";
         ?>
         <tr><th>Spanish</th><th>English</th></tr>
         <tr><td>conocimiento</td><td>knowledge</td></tr>
@@ -57,14 +33,34 @@ and open the template in the editor.
         <tr><td>libro</td><td>book</td></tr>
         <tr><td>bosque</td><td>forest</td></tr>  
         <?php
-      echo "</table>";
+          echo "</table>";
         ?>
+                
+        <br><br><hr><br>Código fuente:<br>
+        <?php
+        highlight_string('
+          <?php
+            echo "<table>";
+          ?>
+          <tr><th>Spanish</th><th>English</th></tr>
+          <tr><td>conocimiento</td><td>knowledge</td></tr>
+          <tr><td>amor</td><td>love</td></tr>
+          <tr><td>felicidad</td><td>happiness</td></tr>
+          <tr><td>agua</td><td>water</td></tr>
+          <tr><td>aire</td><td>air</td></tr>
+          <tr><td>pastel</td><td>cake</td></tr>
+          <tr><td>dinero</td><td>money</td></tr>
+          <tr><td>libro</td><td>book</td></tr>
+          <tr><td>bosque</td><td>forest</td></tr>  
+          <?php
+            echo "</table>";
+          ?>
         ');
-    ?>
+        ?>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
