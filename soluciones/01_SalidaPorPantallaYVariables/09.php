36a6b832bb696a95f7a90dcebbe9b5bab8293152
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-31 09:52:05 +0100

    Revisando soluciones del capítulo 1.

--- soluciones/01_SalidaPorPantallaYVariables/09.php
+++ soluciones/01_SalidaPorPantallaYVariables/09.php
@@ -1,46 +1,42 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>1. Salida por pantalla y variables
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>1. Salida por pantalla y variables
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        $pesetas = 1000;
-        echo $pesetas, " pesetas son ", round($pesetas / 166.386, $precision=2), " euros.";
-    ?>
+        <?php
+          $pesetas = 1000;
+          echo $pesetas, " pesetas son ", round($pesetas / 166.386, $precision=2), " euros.";
+        ?>
                 
-                <br><br><hr><br>Código fuente:<br>
-                <?php
-    highlight_string('
-    <?php
-  $pesetas = 1000;
-  echo $pesetas, " pesetas son ", round($pesetas / 166.386, $precision=2), " euros.";
-    ?>
-    ');
-                ?>
+        <br><br><hr><br>Código fuente:<br>
+        <?php
+        highlight_string('
+        <?php
+          $pesetas = 1000;
+          echo $pesetas, " pesetas son ", round($pesetas / 166.386, $precision=2), " euros.";
+        ?>
+        ');
+        ?>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
