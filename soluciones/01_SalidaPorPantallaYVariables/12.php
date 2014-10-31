ea6800aa5b6914283189d9395f5036b584fb5dd9
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-31 09:59:58 +0100

    Revisando soluciones del capítulo 1.

--- soluciones/01_SalidaPorPantallaYVariables/12.php
+++ soluciones/01_SalidaPorPantallaYVariables/12.php
@@ -1,53 +1,52 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>1. Salida por pantalla y variables
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>1. Salida por pantalla y variables
         </h2>
       </div>
 
       <div id="content">
-    <p style="font-family: courier">
-    <?php
-        echo "&nbsp;&nbsp;&nbsp;*********<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;*<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;*<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-    ?>
-    </p>
-
-                
-                <br><br><hr><br>Código fuente:<br>
-                <?php
-    highlight_string('
-    <p style="font-family: courier">
-    <?php
-        echo "&nbsp;&nbsp;&nbsp;*********<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;*<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;*<br>";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-    ?>
-    </p>
-    ');
-                ?>
+        <p style="font-family: courier">
+        <?php
+          echo "&nbsp;&nbsp;&nbsp;*********<br>";
+          echo "&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
+          echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;*<br>";
+          echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;*<br>";
+          echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
+        ?>
+        </p>
+              
+        <br><br><hr><br>Código fuente:<br>
+        <?php
+          highlight_string('
+            <p style="font-family: courier">
+            <?php
+              echo "&nbsp;&nbsp;&nbsp;*********<br>";
+              echo "&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
+              echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;*<br>";
+              echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;*<br>";
+              echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
+            ?>
+            </p>
+          ');
+        ?>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
