dd5105b16ca7ff50babbc4960768a350189b715d
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-06 09:06:49 +0100

    Revisando soluciones del capítulo 2.

--- soluciones/02_RecogidaDeDatosPorTeclado/10.php
+++ soluciones/02_RecogidaDeDatosPorTeclado/10.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>2. Recogida de datos por teclado
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>2. Recogida de datos por teclado
         </h2>
       </div>
 
       <div id="content">
-    <h2>Conversor de Mb a Kb</h2>
-    <form action="10result.php" method="post">
-        Megabytes <input type="text" name="mb"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        <h2>Conversor de Mb a Kb</h2>
+        <form action="10result.php" method="post">
+          Megabytes <input type="number" name="mb" min="0" autofocus><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
--- soluciones/02_RecogidaDeDatosPorTeclado/10result.php
+++ soluciones/02_RecogidaDeDatosPorTeclado/10result.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>2. Recogida de datos por teclado
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>2. Recogida de datos por teclado
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        echo $_POST['mb'], " Mb son ", $_POST['mb']*1024, " Kb.";
-    ?>
+        <?php
+          echo $_POST['mb'], " Mb son ", $_POST['mb'] * 1024, " Kb.";
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
