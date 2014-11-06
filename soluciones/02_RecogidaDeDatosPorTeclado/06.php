028a88f1f5d1a375794cf8fb8b0fcb135f1e86d4
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-06 08:50:46 +0100

    Revisando soluciones del capítulo 2.

--- soluciones/02_RecogidaDeDatosPorTeclado/06.php
+++ soluciones/02_RecogidaDeDatosPorTeclado/06.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>2. Recogida de datos por teclado
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>2. Recogida de datos por teclado
         </h2>
       </div>
 
       <div id="content">
-    <h2>Cálculo del área de un triángulo</h2>
-    <form action="06result.php" method="post">
-        Anchura en cm <input type="text" name="ancho"><br>
-        Altura en cm <input type="text" name="alto"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        <h2>Cálculo del área de un triángulo</h2>
+        <form action="06result.php" method="post">
+          Anchura en cm <input type="number" name="ancho" min="0" autofocus><br>
+          Altura en cm <input type="number" name="alto" min="0"><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
