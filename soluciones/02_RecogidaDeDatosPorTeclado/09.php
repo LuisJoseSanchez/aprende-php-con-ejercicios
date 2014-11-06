27b867642e692e08e085a802b0f3ab66d4bff74f
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-06 09:03:34 +0100

    Revisando soluciones del capítulo 2.

--- soluciones/02_RecogidaDeDatosPorTeclado/09.php
+++ soluciones/02_RecogidaDeDatosPorTeclado/09.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>2. Recogida de datos por teclado
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>2. Recogida de datos por teclado
         </h2>
       </div>
 
       <div id="content">
-    <h2>Volumen de un cono</h2>
-    <form action="09result.php" method="post">
-        Radio (en cm) <input type="text" name="r"><br>
-        Altura (en cm) <input type="text" name="h"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        <h2>Volumen de un cono</h2>
+        <form action="09result.php" method="post">
+            Radio (en cm) <input type="number" name="r" min="0" autofocus><br>
+            Altura (en cm) <input type="number" name="h" min="0"><br>
+            <input type="submit" value="Aceptar">
+        </form>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
--- soluciones/02_RecogidaDeDatosPorTeclado/09result.php
+++ soluciones/02_RecogidaDeDatosPorTeclado/09result.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>2. Recogida de datos por teclado
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>2. Recogida de datos por teclado
         </h2>
       </div>
 
       <div id="content">
-    <?php
+      <?php
         echo "El volumen del cono es ";
-        $v = (1/3)*Math.pi()*$_POST['r']*$_POST['r']*$_POST['h'];
-        echo round($v, $precision=2), " cm<sup>2</sup>.";
-    ?>
+        $v = (1 / 3) * Math.pi() * $_POST['r'] * $_POST['r'] * $_POST['h'];
+        echo round($v, $precision = 2), " cm<sup>2</sup>.";
+      ?>
+      <br><br>
+      <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
