ad67b7c2734f08ae104d6906ea6a9a471b7fe900
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-06 09:00:10 +0100

    Revisando soluciones del capítulo 2.

--- soluciones/02_RecogidaDeDatosPorTeclado/08result.php
+++ soluciones/02_RecogidaDeDatosPorTeclado/08result.php
@@ -1,32 +1,30 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>2. Recogida de datos por teclado
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>2. Recogida de datos por teclado
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        echo "Su salario semanal es ";
-        echo $_POST['horas']*12, " euros.";
-    ?>
+        <?php
+          echo "Su salario semanal es ";
+          echo $_POST['horas'] * 12, " euros.";
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
