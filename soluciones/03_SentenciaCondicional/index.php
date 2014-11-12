40c430814bb540bf3670d2dee3df1232fe8f0266
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 08:54:05 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/index.php
+++ soluciones/03_SentenciaCondicional/index.php
@@ -1,151 +1,156 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-	<div id="container">
-	    <div id="header">
-		    <h1>
-			    APRENDE PHP CON EJERCICIOS
-		    </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
-		    <h2>
-                        <br>3. Sentencia condicional
-		    </h2>
-	    </div>
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
+      <div id="header">
+        <h1>
+          APRENDE PHP CON EJERCICIOS
+        </h1>
+        <h2>
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
+        </h2>
+      </div>
 
-	    <div id="content">
-		<ol>
-		    <li>
-			Escribe un programa que pida por teclado un día de la semana y que diga qué asignatura toca a primera hora ese día.
-			<br /><a href="01.php">&gt;&gt; Solución</a>
-		    </li>
+      <div id="content">
+        <ol>
+          <li>
+            Escribe un programa que pida por teclado un día de la semana y que diga
+            qué asignatura toca a primera hora ese día.
+            <br /><a href="01.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Realiza un programa que pida una hora por teclado y que muestre luego buenos días, buenas tardes o buenas noches
-                        según la hora. Se utilizarán los tramos de 6 a 12, de 13 a 20 y de 21 a 5. respectivamente. Sólo se tienen en
-                        cuenta las horas, los minutos no se deben introducir por teclado.
-			<br /><a href="02.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Realiza un programa que pida una hora por teclado y que muestre luego
+            buenos días, buenas tardes o buenas noches según la hora. Se utilizarán
+            los tramos de 6 a 12, de 13 a 20 y de 21 a 5. respectivamente. Sólo se
+            tienen en cuenta las horas, los minutos no se deben introducir por teclado.
+            <br /><a href="02.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Escribe un programa en que dado un número del 1 a 7 escriba el correspondiente nombre del día de la semana.
-		     <br /><a href="03.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Escribe un programa en que dado un número del 1 a 7 escriba el correspondiente
+            nombre del día de la semana.
+           <br /><a href="03.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Vamos a ampliar uno de los ejercicios de la relación anterior para considerar las horas extras. Escribe
-                        un programa que calcule el salario semanal de un trabajador teniendo en cuenta que las horas ordinarias
-                        (40 primeras horas de trabajo) se pagan a 12 euros la hora. A partir de la hora 41, se pagan a 16 euros
-                        la hora.
-		    <br /><a href="04.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Vamos a ampliar uno de los ejercicios de la relación anterior para considerar
+            las horas extras. Escribe un programa que calcule el salario semanal de
+            un trabajador teniendo en cuenta que las horas ordinarias (40 primeras
+            horas de trabajo) se pagan a 12 euros la hora. A partir de la hora 41,
+            se pagan a 16 euros la hora.
+            <br /><a href="04.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-                        Realiza un programa que resuelva una ecuación de primer grado (del tipo <i>ax+b=0</i>).
-		    <br /><a href="05.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Realiza un programa que resuelva una ecuación de primer grado (del tipo
+            <i>ax+b=0</i>).
+            <br /><a href="05.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-                        Realiza un programa que calcule el tiempo que tardará en caer un objeto desde una altura <i>h</i>.
-                        Aplica la fórmula <i>t=&#8730;(2h/g)</i> siendo <i>g=9.81m/s<sup>2</sup></i>
- 
-		    <br /><a href="06.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Realiza un programa que calcule el tiempo que tardará en caer un objeto
+            desde una altura <i>h</i>. Aplica la fórmula <i>t=&#8730;(2h/g)</i>
+            siendo <i>g=9.81m/s<sup>2</sup></i>
+            <br /><a href="06.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Realiza un programa que calcule la media de tres notas.
-		    <br /><a href="07.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Realiza un programa que calcule la media de tres notas.
+            <br /><a href="07.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Amplía el programa anterior para que diga la nota del
-                        boletín (insuficiente, suficiente, bien, notable o
-                        sobresaliente).
-		    <br /><a href="08.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Amplía el programa anterior para que diga la nota del boletín (insuficiente,
+            suficiente, bien, notable o sobresaliente).
+            <br /><a href="08.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Realiza un programa que resuelva una ecuación de segundo grado
-                        (del tipo <i>ax<sup>2</sup>+bx+c=0</i>).
-		    <br /><a href="09.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Realiza un programa que resuelva una ecuación de segundo grado (del tipo
+            <i>ax<sup>2</sup>+bx+c=0</i>).
+            <br /><a href="09.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Escribe un programa que nos diga el horóscopo a partir del día y el mes de nacimiento.
-		    <br /><a href="10.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Escribe un programa que nos diga el horóscopo a partir del día y el mes de nacimiento.
+            <br /><a href="10.php">&gt;&gt; Solución</a>
+          </li>
 
-		    <li>
-			Escribe un programa que dada una hora determinada (horas y minutos),
-                        calcule los segundos que faltan para llegar a la medianoche.
-		    <br /><a href="11.php">&gt;&gt; Solución</a>
-		    </li>
-                    
-                    <li>
-			Realiza un minicuestionario con 10 preguntas tipo test sobre las
-                        asignaturas que se imparten en el curso. Cada pregunta acertada
-                        sumará un punto. El programa mostrará al final la calificación
-                        obtenida. Pásale el minicuestionario a tus compañeros y pídeles
-                        que lo hagan para ver qué tal andan de conocimientos en las
-                        diferentes asignaturas del curso.
-		    <br /><a href="12.php">&gt;&gt; Solución</a>
-		    </li>
-                    <li>
-                        Escribe un programa que ordene tres números enteros introducidos por teclado.
-		    <br /><a href="13.php">&gt;&gt; Solución</a>
-		    </li>
-                    
-                    <li>
-			Realiza un programa que diga si un número introducido por teclado es par
-                        y/o divisible entre 5.
-		    <br /><a href="14.php">&gt;&gt; Solución</a>
-		    </li>                    
+          <li>
+            Escribe un programa que dada una hora determinada (horas y minutos),
+            calcule los segundos que faltan para llegar a la medianoche.
+            <br /><a href="11.php">&gt;&gt; Solución</a>
+          </li>
 
-                    <li>
-			Realiza un programa que nos diga si hay probabilidad de que nuestra pareja
-                        nos está siendo infiel. El programa irá haciendo preguntas que el usuario
-                        contestará con verdadero o falso. Puedes utilizar radio buttons. Cada
-                        pregunta contestada como verdadero sumará 3 puntos. Las preguntas contestadas
-                        con falso no suman puntos. Utiliza el fichero <i>test_infidelidad.txt</i> para
-                        obtener las preguntas y las conclusiones del programa.
-		    <br /><a href="15.php">&gt;&gt; Solución</a>
-		    </li>
-                    <li>
-			Escribe un programa que diga cuál es la última cifra de un número entero
-                        introducido por teclado.
-		    <br /><a href="16.php">&gt;&gt; Solución</a>
-		    </li>
-                    
-                    <li>
-			Escribe un programa que diga cuál es la primera cifra de un
-                        número entero introducido por teclado. Se permiten números
-                        de hasta 5 cifras.
-		    <br /><a href="17.php">&gt;&gt; Solución</a>
-		    </li>
+          <li>
+            Realiza un minicuestionario con 10 preguntas tipo test sobre las
+            asignaturas que se imparten en el curso. Cada pregunta acertada
+            sumará un punto. El programa mostrará al final la calificación
+            obtenida. Pásale el minicuestionario a tus compañeros y pídeles
+            que lo hagan para ver qué tal andan de conocimientos en las diferentes
+            asignaturas del curso.
+            <br /><a href="12.php">&gt;&gt; Solución</a>
+          </li>
 
-                    <li>
-			Realiza un programa que nos diga cuántos dígitos tiene un número
-                        entero que puede ser positivo o negativo. Se permiten números de
-                        hasta 5 dígitos.
-		    <br /><a href="18.php">&gt;&gt; Solución</a>
-		    </li>
-                    
-                    <li>
-			Realiza un programa que diga si un número entero positivo
-                        introducido por teclado es capicúa. Se permiten números
-                        de hasta 5 cifras.
-		    <br /><a href="19.php">&gt;&gt; Solución</a>
-		    </li>                    
-                </ol>
-	    </div>
-	    <div id="footer">
-		    © Luis José Sánchez González
-	    </div>
-	</div>
-    </body>
+          <li>
+            Escribe un programa que ordene tres números enteros introducidos por teclado.
+            <br /><a href="13.php">&gt;&gt; Solución</a>
+          </li>
+
+          <li>
+            Realiza un programa que diga si un número introducido por teclado es par
+            y/o divisible entre 5.
+            <br /><a href="14.php">&gt;&gt; Solución</a>
+          </li>                    
+
+          <li>
+            Realiza un programa que nos diga si hay probabilidad de que nuestra pareja
+            nos está siendo infiel. El programa irá haciendo preguntas que el usuario
+            contestará con verdadero o falso. Puedes utilizar radio buttons. Cada
+            pregunta contestada como verdadero sumará 3 puntos. Las preguntas contestadas
+            con falso no suman puntos. Utiliza el fichero <i>test_infidelidad.txt</i> para
+            obtener las preguntas y las conclusiones del programa.
+            <br /><a href="15.php">&gt;&gt; Solución</a>
+          </li>
+
+          <li>
+            Escribe un programa que diga cuál es la última cifra de un número entero
+            introducido por teclado.
+            <br /><a href="16.php">&gt;&gt; Solución</a>
+          </li>
+
+          <li>
+            Escribe un programa que diga cuál es la primera cifra de un número entero
+            introducido por teclado. Se permiten números de hasta 5 cifras.
+            <br /><a href="17.php">&gt;&gt; Solución</a>
+          </li>
+
+          <li>
+            Realiza un programa que nos diga cuántos dígitos tiene un número entero
+            que puede ser positivo o negativo. Se permiten números de hasta 5 dígitos.
+          <br /><a href="18.php">&gt;&gt; Solución</a>
+          </li>
+
+          <li>
+            Realiza un programa que diga si un número entero positivo introducido
+            por teclado es capicúa. Se permiten números de hasta 5 cifras.
+            <br /><a href="19.php">&gt;&gt; Solución</a>
+          </li>                    
+
+        </ol>
+      </div>
+      
+      <div id="footer">
+        © Luis José Sánchez González
+      </div>
+    </div>
+  </body>
 </html>
