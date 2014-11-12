6321ebacc6b8b9fe7d195431736b4ccc78a99b24
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 09:31:44 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/07.php
+++ soluciones/03_SentenciaCondicional/07.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    Este programa calcula la media de tres notas.
-    <form action="07result.php" method="post">
-        Nota 1 <input type="text" name="nota1"><br>
-        Nota 2 <input type="text" name="nota2"><br>
-        Nota 3 <input type="text" name="nota3"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        Este programa calcula la media de tres notas.
+        <form action="07result.php" method="post">
+          Nota 1 <input type="number" name="nota1" step="0.01" autofocus><br>
+          Nota 2 <input type="number" name="nota2" step="0.01" ><br>
+          Nota 3 <input type="number" name="nota3" step="0.01" ><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
--- soluciones/03_SentenciaCondicional/07result.php
+++ soluciones/03_SentenciaCondicional/07result.php
@@ -1,36 +1,36 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        echo "La nota media es ";
-        echo round(($_POST['nota1'] + $_POST['nota2'] + $_POST['nota3'])/3, $precision=2);
-    ?>
+        <?php
+          echo "La nota media es ";
+          echo round(($_POST['nota1'] + $_POST['nota2'] + $_POST['nota3']) / 3, $precision = 2);
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
