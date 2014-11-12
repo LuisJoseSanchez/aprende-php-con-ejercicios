2540d33fe91818dcacdc2dc0867ab615797966b6
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 09:21:16 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/05.php
+++ soluciones/03_SentenciaCondicional/05.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-                Este programa resuelve ecuaciones de primer grado del tipo <i>ax + b = 0</i>
-    <form action="05result.php" method="post">
-                    <h2><input type="text" name="a" size="4" value="a">x + 
-        <input type="text" name="b" size="4" value="b">
-                    = 0<br></h2>
-        <input type="submit" value="Aceptar">
-    </form>
+        Este programa resuelve ecuaciones de primer grado del tipo <i>ax + b = 0</i>
+        <form action="05result.php" method="post">
+          <h2>
+            <input type="text" name="a" size="4" value="a">x + 
+            <input type="text" name="b" size="4" value="b"> = 0<br>
+          </h2>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
