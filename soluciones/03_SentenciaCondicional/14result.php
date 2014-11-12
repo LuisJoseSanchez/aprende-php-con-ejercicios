a2ef53b428edbeddc502d3230be6b5fe18c3ec8c
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 12:01:28 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/14result.php
+++ soluciones/03_SentenciaCondicional/14result.php
@@ -1,40 +1,47 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-    $n = $_POST['n'];
-    if (($n % 2) == 0)
-        echo "El número introducido es par";
-      else
-        echo "El número introducido es impar";
+        <?php
+          $n = $_POST['n'];
+          
+          if (($n % 2) == 0) {
+            echo "El número introducido es par";
+          } else {
+            echo "El número introducido es impar";
+          }
 
-    if (($n % 5) == 0)
-        echo " y divisible entre 5.";
-      else
-        echo " y no es divisible entre 5.";
-                ?>
+          if (($n % 5) == 0) {
+            echo " y divisible entre 5.";
+          } else {
+            echo " y no es divisible entre 5.";
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a> 
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
