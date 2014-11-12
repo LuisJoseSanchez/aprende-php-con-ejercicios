f18cc8187db7189cc3440553bc4e83ffdf3a1c07
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 09:15:56 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/03result.php
+++ soluciones/03_SentenciaCondicional/03result.php
@@ -1,47 +1,63 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-    $n = $_POST['n'];
-                
-    switch($n) {
-      case 1: $dia  = "lunes"; break;
-      case 2: $dia  = "martes"; break;
-      case 3: $dia  = "miércoles"; break;
-      case 4: $dia  = "jueves"; break;
-      case 5: $dia  = "viernes"; break;
-      case 6: $dia  = "sábado"; break;
-      case 7: $dia  = "domingo"; break;
-      default: $dia  = "Debe introducir un número del 1 al 7";
-    }
-    
-    echo $dia;    ?>
+        <?php
+          $n = $_POST['n'];
+
+          switch($n) {
+            case 1:
+              $dia  = "lunes";
+              break;
+            case 2:
+              $dia  = "martes";
+              break;
+            case 3:
+              $dia  = "miércoles";
+              break;
+            case 4:
+              $dia  = "jueves";
+              break;
+            case 5:
+              $dia  = "viernes";
+              break;
+            case 6:
+              $dia  = "sábado";
+              break;
+            case 7:
+              $dia  = "domingo";
+              break;
+            default:
+              $dia  = "Debe introducir un número del 1 al 7";
+          }
+
+          echo $dia;
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
--- soluciones/03_SentenciaCondicional/04result.php
+++ soluciones/03_SentenciaCondicional/04result.php
@@ -1,43 +1,43 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-	<div id="container">
-	    <div id="header">
-		    <h1>
-			    APRENDE PHP CON EJERCICIOS
-		    </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
-		    <h2>
-                        <br>3. Sentencia condicional
-		    </h2>
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
+      <div id="header">
+        <h1>
+          APRENDE PHP CON EJERCICIOS
+        </h1>
+        <h2>
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
+        </h2>
+      </div>
+
+      <div id="content">
+        <?php
+          $horasTrabajadas  = $_POST['horasTrabajadas'];
+
+          if ($horasTrabajadas  < 40) {
+            $sueldoSemanal = 12 * $horasTrabajadas ;
+          } else {
+            $sueldoSemanal = (40 * 12) + (($horasTrabajadas  - 40) * 16);
+          }
+
+          echo "El sueldo semanal que le corresponde es de $sueldoSemanal euros";
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
 	    </div>
-
-	    <div id="content">
-		<?php
-
-                $horasTrabajadas  = $_POST['horasTrabajadas'];
-
-                    if ($horasTrabajadas  < 40)
-                                    $sueldoSemanal = 12 * $horasTrabajadas ;
-                            else
-                                    $sueldoSemanal = (40 * 12) + (($horasTrabajadas  - 40) * 16);		
-
-                echo "El sueldo semanal que le corresponde es de $sueldoSemanal euros";
-		?>
-	    </div>
+      
 	    <div id="footer">
 		    © Luis José Sánchez González
 	    </div>
-	</div>
-    </body>
+      
+    </div>
+  </body>
 </html>
