dfd61e2cc28529af03c85cbe955626a5e7d3a29b
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 09:06:21 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/02.php
+++ soluciones/03_SentenciaCondicional/02.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    Por favor, introduzca una hora del día (0 - 23):
-    <form action="02result.php" method="post">
-        <input type="text" name="hora"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        Por favor, introduzca una hora del día (0 - 23):
+        <form action="02result.php" method="post">
+          <input type="text" name="hora" autofocus><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
--- soluciones/03_SentenciaCondicional/02result.php
+++ soluciones/03_SentenciaCondicional/02result.php
@@ -1,47 +1,51 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-                    $hora = $_POST['hora'];
+        <?php
+          $hora = $_POST['hora'];
 
-                    if (($hora >= 6) && ($hora <= 12))
-                            echo "Buenos días";
+          if (($hora >= 6) && ($hora <= 12)) {
+            echo "Buenos días";
+          }
 
-                    if (($hora >= 13) && ($hora <= 20))
-                            echo "Buenas tardes";
+          if (($hora >= 13) && ($hora <= 20)) {
+            echo "Buenas tardes";
+          }
 
-                    if ((($hora >= 21) && ($hora < 24)) || (($hora <= 5) && ($hora >= 0)))
-                            echo "Buenas noches";
+          if ((($hora >= 21) && ($hora < 24)) || (($hora <= 5) && ($hora >= 0))) {
+            echo "Buenas noches";
+          }
 
-                    if (($hora >= 24) || ($hora < 0))
-                            echo "La hora introducida no es correcta.";
-    ?>
+          if (($hora >= 24) || ($hora < 0)) {
+            echo "La hora introducida no es correcta.";
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
