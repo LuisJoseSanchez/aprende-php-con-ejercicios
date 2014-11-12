f728a2850150ffaddd8965aa7be49f759f7e0698
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 12:38:49 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/18result.php
+++ soluciones/03_SentenciaCondicional/18result.php
@@ -1,40 +1,62 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        $n = $_POST['n'];
-        
-        if ($n < 0) $n = -$n;
-        if ( $n < 10 ) $digitos = 1;
-        if (( $n >= 10 ) && ( $n < 100 )) $digitos = 2;
-        if (( $n >= 100 ) && ( $n < 1000 )) $digitos = 3;
-        if (( $n >= 1000 ) && ( $n < 10000 )) $digitos = 4;
-        if ( $n >= 10000 ) $digitos = 5;
+        <?php
+          $n = $_POST['n'];
 
-        echo "El número introducido tiene $digitos dígitos.";
-    ?>
+          // si el número es negativo, se le quita el signo
+          if ($n < 0){
+            $n = -$n;
+          }
+          
+          if ( $n < 10 ) {
+            $digitos = 1;
+          }
+          
+          if (( $n >= 10 ) && ( $n < 100 )) {
+            $digitos = 2;
+          }
+          
+          if (( $n >= 100 ) && ( $n < 1000 )) {
+            $digitos = 3;
+          }
+          
+          if (( $n >= 1000 ) && ( $n < 10000 )) {
+            $digitos = 4;
+          }
+          
+          if ( $n >= 10000 ) {
+            $digitos = 5;
+          }
+
+          echo "El número introducido tiene $digitos dígitos.";
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
