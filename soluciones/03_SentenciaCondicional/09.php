c7470946638a542dade5c066d198df152c7beb20
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 10:09:56 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/09.php
+++ soluciones/03_SentenciaCondicional/09.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-                Este programa resuelve ecuaciones de segundo grado del tipo <i>ax<sup>2</sup> + bx + c = 0</i>
-    <form action="09result.php" method="post">
-                    <h2>
-                    <input type="text" name="a" size="4" value="a">x<sup>2</sup> + 
-        <input type="text" name="b" size="4" value="b">x +
-                    <input type="text" name="c" size="4" value="c">
-                    = 0<br></h2>
-        <input type="submit" value="Aceptar">
-    </form>
+        Este programa resuelve ecuaciones de segundo grado del tipo <i>ax<sup>2</sup> + bx + c = 0</i>
+        <form action="09result.php" method="post">
+          <h2>
+            <input type="number" name="a" step="0.001" autofocus>x<sup>2</sup> + 
+            <input type="number" name="b" step="0.001">x +
+            <input type="number" name="c" step="0.001"> = 0<br>
+          </h2>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
--- soluciones/03_SentenciaCondicional/09result.php
+++ soluciones/03_SentenciaCondicional/09result.php
@@ -1,74 +1,81 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-                    $a = $_POST['a'];
-                    $b = $_POST['b'];
-                    $c = $_POST['c'];
+        <?php
+          $a = $_POST['a'];
+          $b = $_POST['b'];
+          $c = $_POST['c'];
+
+          
+          // 0x^2 + 0x + 0 = 0
+
+          if (($a == 0) && ($b == 0) && ($c == 0)) {
+            echo  "La ecuación tiene infinitas soluciones.";
+          }
+
+          
+          // 0x^2 + 0x + c = 0  con c distinto de 0
+
+          if (($a == 0) && ($b == 0) && ($c != 0)) {
+            echo  "La ecuación no tiene solución.";
+          }
+
+          
+          // ax^2 + bx + 0 = 0  con a y b distintos de 0
+
+          if (($a != 0) && ($b != 0) && ($c == 0)) {
+            echo  "x<sub>1</sub> = 0";
+            echo  "<br>x<sub>2</sub> = ", (-$b / $a);
+          }
 
 
-                    // 0x^2 + 0x + 0 = 0
+          // 0x^2 + bx + c = 0  con b y c distintos de 0
 
-                    if (($a == 0) && ($b == 0) && ($c == 0))
-                            echo  "La ecuación tiene infinitas soluciones.";
+          if (($a == 0) && ($b != 0) && ($c != 0)) {
+                  echo  "x<sub>1</sub> = x<sub>2</sub> = ", (-$c / $b);
+          }
 
+          
+          // ax^2 + bx + c = 0  con a, b y c distintos de 0
 
-                    // 0x^2 + 0x + c = 0  con c distinto de 0
+          if (($a != 0) && ($b != 0) && ($c != 0)) {  
 
-                    if (($a == 0) && ($b == 0) && ($c != 0))
-                            echo  "La ecuación no tiene solución.";
+            $discriminante = ($b * $b) - (4 * $a * $c);
 
-
-                    // ax^2 + bx + 0 = 0  con a y b distintos de 0
-
-                    if (($a != 0) && ($b != 0) && ($c == 0)) {
-                            echo  "x1 = 0";
-                            echo  "<br>x2 = ", (-$b / $a);
-                            }
-
-
-                    // 0x^2 + bx + c = 0  con b y c distintos de 0
-
-                    if (($a == 0) && ($b != 0) && ($c != 0))
-                            echo  "x1 = x2 = ", (-$c / $b);
-
-
-                    // ax^2 + bx + c = 0  con a, b y c distintos de 0
-
-                    if (($a != 0) && ($b != 0) && ($c != 0)) {  
-
-                            $discriminante = $b*$b - (4 * $a * $c);
-
-                            if ($discriminante < 0)
-                                            echo  "La ecuación no tiene soluciones reales";
-                                    else {
-                                            echo  "x1 = ", (-$b + sqrt($discriminante))/(4 * $a * $c);
-                                            echo  "<br>x2 = ", (-$b - sqrt($discriminante))/(4 * $a * $c);
-                                    }
-                            }
-    ?>
+            if ($discriminante < 0) {
+              echo  "La ecuación no tiene soluciones reales";
+            } else {
+              echo  "x<sub>1</sub> = ", (-$b + sqrt($discriminante)) / (4 * $a * $c);
+              echo  "<br>x<sub>2</sub> = ", (-$b - sqrt($discriminante)) / (4 * $a * $c);
+            }
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
