db8588ed591339a57bbf84027e515e6ede8a93a8
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 12:31:24 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/17.php
+++ soluciones/03_SentenciaCondicional/17.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    Por favor, introduzca un número entero (como máximo de 5 cifras): 
-    <form action="17result.php" method="post">
-        <input type="text" name="n"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        Por favor, introduzca un número entero (como máximo de 5 cifras): 
+        <form action="17result.php" method="post">
+          <input type="number" min="-99999" max="99999" name="n" autofocus><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
--- soluciones/03_SentenciaCondicional/17result.php
+++ soluciones/03_SentenciaCondicional/17result.php
@@ -1,38 +1,62 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        $n = $_POST['n'];
-        if ( $n < 10 ) $primera = n;
-        if (( $n >= 10 ) && ( $n < 100 )) $primera = $n / 10;
-        if (( $n >= 100 ) && ( $n < 1000 )) $primera = $n / 100;
-        if (( $n >= 1000 ) && ( $n < 10000 )) $primera = $n / 1000;
-        if ( $n >= 10000 ) $primera = $n / 10000;
+        <?php
+          $n = $_POST['n'];
 
-        echo "La primera cifra del número introducido es ", floor($primera);
-    ?>
+          // si el número 
+          if ($n<0){
+            $n = -$n;
+          }
+
+          if ( $n < 10 ) {
+            $primera = n;
+          }
+
+          if (( $n >= 10 ) && ( $n < 100 )) {
+            $primera = $n / 10;
+          }
+
+          if (( $n >= 100 ) && ( $n < 1000 )) {
+            $primera = $n / 100;
+          }
+
+          if (( $n >= 1000 ) && ( $n < 10000 )) {
+            $primera = $n / 1000;
+          }
+
+          if ( $n >= 10000 ) {
+            $primera = $n / 10000;
+          }
+
+          echo "La primera cifra del número introducido es ", floor($primera);
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
