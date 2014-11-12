92f8eef5f870bc7319282fc888b4f50b863c3048
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 11:00:00 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/11.php
+++ soluciones/03_SentenciaCondicional/11.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    A continuación deberá introducir una hora del día.<br>
-    <form action="11result.php" method="post">
-        Hora <input type="text" name="hora"><br>
-                    Minuto <input type="text" name="minuto"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        A continuación deberá introducir una hora del día.<br>
+        <form action="11result.php" method="post">
+          Hora <input type="number" name="hora" autofocus><br>
+          Minuto <input type="number" name="minuto"><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
--- soluciones/03_SentenciaCondicional/11result.php
+++ soluciones/03_SentenciaCondicional/11result.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-    $hora = $_POST['hora'];
-                $minuto = $_POST['minuto'];
+        <?php
+          $hora = $_POST['hora'];
+          $minuto = $_POST['minuto'];
 
-    $segundosTranscurridos = ($hora * 3600) + ($minuto * 60);
-    $segundosHastaMedianoche = (24 * 3600) - $segundosTranscurridos;
-            
-    echo "Desde las $hora:$minuto hasta la medianoche faltan ";
-                echo "$segundosHastaMedianoche segundos.";
-                ?>
+          $segundosTranscurridos = ($hora * 3600) + ($minuto * 60);
+          $segundosHastaMedianoche = (24 * 3600) - $segundosTranscurridos;
+
+          echo "Desde las $hora:$minuto hasta la medianoche faltan ";
+          echo "$segundosHastaMedianoche segundos.";
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
