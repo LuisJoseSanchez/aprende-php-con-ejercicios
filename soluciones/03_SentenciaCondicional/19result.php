6c889daac9a42ae9c0fe7b3ba7f6223cc0967a67
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 12:47:03 +0100

    Revisandas las soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/19result.php
+++ soluciones/03_SentenciaCondicional/19result.php
@@ -1,58 +1,75 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-    $n = $_POST['n'];
-    
-    $capicua = false;
-    
-    // número de una cifra
-    if ( $n < 10 )
-      $capicua = true;
-      
-    // número de dos cifras
-    if (( $n >= 10 ) && ( $n < 100 ))
-      if ( floor($n / 10) == ($n % 10) ) $capicua = true;
+        <?php
+          $n = $_POST['n'];
 
-    // número de tres cifras
-    if (( $n >= 100 ) && ( $n < 1000 ))
-       if ( floor($n / 100) == ($n % 10) ) $capicua = true;
+          $capicua = false;
 
-    // número de cuatro cifras
-    if (( $n >= 1000 ) && ( $n < 10000 ))
-       if ((floor( $n / 1000 ) == ($n % 10) ) && ( (floor( $n / 100 ) % 10 )== (floor( $n / 10) % 10 ))) $capicua = true;
-    
-    // número de cinco cifras
-    if ( $n >= 10000)
-      if ((floor( $n / 10000 ) == ($n % 10) ) && ( ( (floor($n / 1000) % 10) ) == (floor( $n / 10) % 10))) $capicua = true;
-    
-    if ( $capicua )
-        echo "El número introducido es capicúa.";
-      else
-        echo "El número introducido no es capicúa.";
-    ?>
+          // número de una cifra
+          if ( $n < 10 ) {
+            $capicua = true;
+          }
+
+          // número de dos cifras
+          if (( $n >= 10 ) && ( $n < 100 )) {
+            if ( floor($n / 10) == ($n % 10) ) {
+              $capicua = true;
+            }
+          }
+
+          // número de tres cifras
+          if (( $n >= 100 ) && ( $n < 1000 )) {
+            if ( floor($n / 100) == ($n % 10) ) {
+              $capicua = true;
+            }
+          }
+
+          // número de cuatro cifras
+          if (( $n >= 1000 ) && ( $n < 10000 )) {
+            if ((floor( $n / 1000 ) == ($n % 10) ) && ( (floor( $n / 100 ) % 10 )== (floor( $n / 10) % 10 ))) {
+              $capicua = true;
+            }
+          }
+
+          // número de cinco cifras
+          if ( $n >= 10000) {
+            if ((floor( $n / 10000 ) == ($n % 10) ) && ( ( (floor($n / 1000) % 10) ) == (floor( $n / 10) % 10))) {
+              $capicua = true;
+            }
+          }
+
+          if ( $capicua ) {
+            echo "El número introducido es capicúa.";
+          } else {
+            echo "El número introducido no es capicúa.";
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
