eca570d464af309d1eebe9536ef1aa5b764e13d6
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 09:27:00 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/06.php
+++ soluciones/03_SentenciaCondicional/06.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <h2>Cálculo del tiempo de caída de un objeto</h2>
-                Por favor, introduzca la altura (en metros) desde la que cae el objeto:
-    <form action="06result.php" method="post">
-        <input type="text" name="h"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        <h2>Cálculo del tiempo de caída de un objeto</h2>
+        Por favor, introduzca la altura (en metros) desde la que cae el objeto:
+        <form action="06result.php" method="post">
+          <input type="number" name="h" min="0" step="0.01" autofocus><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
