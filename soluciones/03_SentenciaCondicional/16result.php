9e27d8c822387cc868a96f62afab957fa8ba9fb2
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 12:18:06 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/16result.php
+++ soluciones/03_SentenciaCondicional/16result.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        $n = $_POST['n'];
-        echo "La última cifra de $n es ", $n%10;
-    ?>
+        <?php
+            $n = $_POST['n'];
+            echo "La última cifra de $n es ", $n % 10;
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
