2f8fd8ebf3ff6abe2ffed6d7a2b01724607d2055
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 11:54:54 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/13.php
+++ soluciones/03_SentenciaCondicional/13.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    Este programa ordena tres números.<br>
-    <form action="13result.php" method="post">
-        Número 1 <input type="text" name="a"><br>
-                    Número 2 <input type="text" name="b"><br>
-                    Número 3 <input type="text" name="c"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        Este programa ordena tres números.<br>
+        <form action="13result.php" method="post">
+          Número 1 <input type="number" name="a"><br>
+          Número 2 <input type="number" name="b"><br>
+          Número 3 <input type="number" name="c"><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
--- soluciones/03_SentenciaCondicional/13result.php
+++ soluciones/03_SentenciaCondicional/13result.php
@@ -1,58 +1,62 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-                
-    $a = $_POST['a'];
-                $b = $_POST['b'];
-                $c = $_POST['c'];
+        <?php
 
-    // ordenación de los dos primeros números
-    if ($a > $b) {
-      $aux = $a;
-      $a = $b;
-      $b = $aux;
-    }
-    
-    // ordenación de los dos últimos números
-    if ($b > $c) {
-      $aux = $b;
-      $b = $c;
-      $c = $aux;
-    }
-    
-    // se vuelven a ordenar los dos primeros
-    if ($a > $b) {
-      $aux = $a;
-      $a = $b;
-      $b = $aux;
-    }
-        
-    echo "Los números introducidos ordenados de menor a mayor son $a, $b y $c."; 
+          $a = $_POST['a'];
+          $b = $_POST['b'];
+          $c = $_POST['c'];
 
-    ?>
+          // ordenación de los dos primeros números
+          if ($a > $b) {
+            $aux = $a;
+            $a = $b;
+            $b = $aux;
+          }
+
+          // ordenación de los dos últimos números
+          if ($b > $c) {
+            $aux = $b;
+            $b = $c;
+            $c = $aux;
+          }
+
+          // se vuelven a ordenar los dos primeros
+          if ($a > $b) {
+            $aux = $a;
+            $a = $b;
+            $b = $aux;
+          }
+
+          echo "Los números introducidos ordenados de menor a mayor son $a, $b y $c."; 
+
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>       
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
