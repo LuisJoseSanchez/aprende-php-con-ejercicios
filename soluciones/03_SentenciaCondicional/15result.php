9c02568b585f5174799d7e1d09ad509c95d83b4f
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-12 12:13:05 +0100

    Revisando soluciones del capítulo 3.

--- soluciones/03_SentenciaCondicional/15result.php
+++ soluciones/03_SentenciaCondicional/15result.php
@@ -1,50 +1,57 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>3. Sentencia condicional
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>3. Sentencia condicional
         </h2>
       </div>
 
       <div id="content">
-    <?php
-        $puntos = 0;
-        foreach($_POST as $respuesta)
-      $puntos += $respuesta;
-      
-        // Muestra el resultado del test
+        <?php
+          $puntos = 0;
+          
+          foreach($_POST as $respuesta) {
+            $puntos += $respuesta;
+          }
 
-        if ( $puntos <= 10 )
-          echo "¡Enhorabuena! tu pareja parece ser totalmente fiel.";
+          // Muestra el resultado del test
 
-        if ( ($puntos > 11 ) && ($puntos <= 22) ) {
-          echo "Quizás exista el peligro de otra persona en su vida o en su mente,";
-          echo "aunque seguramente será algo sin importancia. No bajes la guardia.";
-        }
+          if ( $puntos <= 10 ) {
+            echo "¡Enhorabuena! tu pareja parece ser totalmente fiel.";
+          }
 
-        if ( $puntos >= 22 ) {
-          echo "Tu pareja tiene todos los ingredientes para estar viviendo un ";
-          echo "romance con otra persona. Te aconsejamos que indagues un poco más ";
-          echo "y averigües qué es lo que está pasando por su cabeza.";
-        }
+          if ( ($puntos > 11 ) && ($puntos <= 22) ) {
+            echo "Quizás exista el peligro de otra persona en su vida o en su mente,";
+            echo "aunque seguramente será algo sin importancia. No bajes la guardia.";
+          }
 
-    ?>
+          if ( $puntos >= 22 ) {
+            echo "Tu pareja tiene todos los ingredientes para estar viviendo un ";
+            echo "romance con otra persona. Te aconsejamos que indagues un poco más ";
+            echo "y averigües qué es lo que está pasando por su cabeza.";
+          }
+
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
