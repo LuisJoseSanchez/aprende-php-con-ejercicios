fc8514c1786c22b423dc08ea969bc94892eed044
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-13 12:33:36 +0100

    Revisando las soluciones del capítulo 4.

--- soluciones/04_Bucles/03.php
+++ soluciones/04_Bucles/03.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-                    $i = 0;
-                    do {
-                        echo "$i  ";
-                        $i++;
-                    } while($i < 101);
-                ?>
+        <?php
+          $i = 0;
+          do {
+              echo "$i  ";
+              $i +=5;
+          } while($i < 101);
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+      
+    </div>
+  </body>
 </html>
