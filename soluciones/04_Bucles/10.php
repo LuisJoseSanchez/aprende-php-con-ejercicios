6163001a10057477a0c08d22b3d2b82b89abb23f
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-19 23:08:49 +0100

    Revisando las soluciones del capítulo 4.

--- soluciones/04_Bucles/10.php
+++ soluciones/04_Bucles/10.php
@@ -1,58 +1,50 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                Este programa calcula la media de los números positivos introducidos.<br>
-                Vaya introduciendo números (puede parar introduciendo un número negativo).<br>
-                <?php
-        $n = $_POST['n'];
-        $total = $_POST['total'];
-        $cuentaNumeros = $_POST['cuentaNumeros'];
-        
-                    if (isset($n)) {
-                        if ($n < 0) {
-                            echo "<br><br>La media de los números introducidos es ";
-          $media = $total/$cuentaNumeros;
-          echo $media."<br>";
-                        } else {
-                            $total += $n;
-                            $cuentaNumeros++;
-                            echo "<form action=\"10.php\" method=\"post\">";
-                            echo "<input type=\"text\" name=\"n\">";
-                            echo "<input type=\"hidden\" name=\"total\" value=\"$total\">";
-                            echo "<input type=\"hidden\" name=\"cuentaNumeros\" value=\"$cuentaNumeros\">";
-                            echo "<input type=\"submit\" value=\"Aceptar\">";
-                            echo "</form>";
-                        }
+        Este programa calcula la media de los números positivos introducidos.<br>
+        Vaya introduciendo números (puede parar introduciendo un número negativo).<br>
+        <?php
+          $n = $_POST['n'];
+          $total = $_POST['total'];
+          $cuentaNumeros = $_POST['cuentaNumeros'];
 
-                    } else {
-      $total = 0;
-      $cuentaNumeros = 0;
-                            echo "<form action=\"10.php\" method=\"post\">";
-                            echo "<input type=\"text\" name=\"n\">";
-                            echo "<input type=\"hidden\" name=\"total\" value=\"$total\">";
-                            echo "<input type=\"hidden\" name=\"cuentaNumeros\" value=\"$cuentaNumeros\">";
-                            echo "<input type=\"submit\" value=\"Aceptar\">";
-                            echo "</form>";
-        }
-    ?>
+          if (!isset($n) || ($n > 0)) {
+            $total += $n;
+            $cuentaNumeros++;
+            ?>
+            <form action="10.php" method="post">
+              <input type="number" name="n" autofocus>
+            <input type="hidden" name="total" value="<?php echo $total; ?>">
+            <input type="hidden" name="cuentaNumeros" value="<?php echo $cuentaNumeros; ?>">
+            <input type="submit" value="Aceptar">
+            </form>   
+          <?php
+          } else {
+          ?>
+            <br><br>La media de los números introducidos es <?php echo $total / ($cuentaNumeros - 1); ?>
+            <br><br>
+            <a href="index.php">>> Volver</a>
+          <?php
+          }
+          ?>
 
       </div>
       <div id="footer">
