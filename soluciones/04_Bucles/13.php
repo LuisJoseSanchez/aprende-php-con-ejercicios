354c3b251bdfd34052f57d8fcc4fb19ab028df89
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-19 23:55:05 +0100

    Revisando las soluciones del capítulo 4.

--- soluciones/04_Bucles/13.php
+++ soluciones/04_Bucles/13.php
@@ -1,66 +1,67 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-        $i = $_POST['i'];
-        $n = $_POST['n'];
-        $positivos = $_POST['positivos'];
-        $negativos = $_POST['negativos'];
+        <?php
+          $cuentaNumeros = $_POST['cuentaNumeros'];
+          $n = $_POST['n'];
+          $positivos = $_POST['positivos'];
 
-        if (!isset($i)) {
-          $i = 1;
-          $positivos = 0;
-          $negativos = 0;
-        } else {
-          $i++;
-          if ($n < 0)
-            $negativos++;
-        else
+          if (!isset($n)) {
+            $cuentaNumeros = 1;
+            $positivos = 0;
+          } else {
+            $cuentaNumeros++;   
+          }
+          
+          if ($n > 0) {
             $positivos++;
-        }
-        
-        if ($i == 1) {
-      echo "Este programa lee una lista de diez números y determina cuántos son positivos, ";
-      echo " y cuántos son negativos.<br>";
-        }
+          }
 
-        if ($i < 11) {
-      echo "<form action=\"13.php\" method=\"post\">";
-                        echo "Número $i <input type=\"text\" name=\"n\">";
-      echo "<input type=\"hidden\" name=\"i\" value=\"$i\">";
-                        echo "<input type=\"hidden\" name=\"positivos\" value=\"$positivos\">";
-      echo "<input type=\"hidden\" name=\"negativos\" value=\"$negativos\">";
-                        echo "<input type=\"submit\" value=\"Aceptar\">";
-                        echo "</form>";
-        }
-        
-        if ($i == 11) {
-      echo "Se han introducido $positivos números positivos y $negativos números negativos.<br>";
-        }
-    ?>
+          if ($cuentaNumeros == 1) {
+            echo "Este programa lee una lista de diez números y determina cuántos son positivos, ";
+            echo " y cuántos son negativos.<br>";
+          }
 
+          if ($cuentaNumeros < 11) {
+          ?>
+            <form action="13.php" method="post">
+              Número <?php echo $cuentaNumeros; ?> <input type="number" name="n" autofocus>
+              <input type="hidden" name="cuentaNumeros" value="<?php echo $cuentaNumeros; ?>">
+              <input type="hidden" name="positivos" value="<?php echo $positivos; ?>">
+              <input type="submit" value="Aceptar">
+            </form>
+          <?php
+          }
+
+          if ($cuentaNumeros == 11) {
+            echo "Se han introducido $positivos números positivos y ", (10 - $positivos), " números negativos.<br>";
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
--- soluciones/04_Bucles/14.php
+++ soluciones/04_Bucles/14.php
@@ -1,55 +1,63 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-        $base = $_POST['base'];
-        $exponente = $_POST['exponente'];
-        
-                    if (!isset($base)) {
-      echo "Este programa calcula la potencia a partir de la base y el exponente.<br>";
-      echo "<form action=\"14.php\" method=\"post\">";
-                        echo "Base: <input type=\"text\" name=\"base\" size=\"5\"><br>";
-      echo "Exponente: <input type=\"text\" name=\"exponente\" size=\"5\"><br>";
-                        echo "<input type=\"submit\" value=\"Aceptar\">";
-                        echo "</form>";
-                    } else {
-      $potencia = 1;
-      
-      if ($exponente == 0)
-          $potencia = 1;
-        
-      if ($exponente > 0)
-          for ($i = 0; $i < $exponente; $i++)
-        $potencia *= $base;
-        
-      if ($exponente < 0) {
-          for ($i = 0; $i < -$exponente; $i++)
-        $potencia *= $base;
-            
-          $potencia = 1/$potencia;
-      }
+        <?php
+          $base = $_POST['base'];
+          $exponente = $_POST['exponente'];
 
-      echo "$base<sup>$exponente</sup> = $potencia";
-        }
-    ?>
+          if (!isset($base)) {
+          ?>  
+            Este programa calcula la potencia a partir de la base y el exponente.<br>
+            <form action="14.php" method="post">
+              Base: <input type="number" name="base" autofocus><br>
+              Exponente: <input type="number" name="exponente"><br>
+              <input type="submit" value="Aceptar">
+            </form>
+          <?php
+          } else {
+            $potencia = 1;
+
+            if ($exponente == 0) {
+              $potencia = 1;
+            }
+
+            if ($exponente > 0) {
+              for ($i = 0; $i < $exponente; $i++) {
+                $potencia *= $base;
+              }
+            }
+
+            if ($exponente < 0) {
+              for ($i = 0; $i < -$exponente; $i++) {
+                $potencia *= $base;
+              }
+
+              $potencia = 1 / $potencia;
+            }
+
+            echo "$base<sup>$exponente</sup> = $potencia";
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
 
       </div>
       <div id="footer">
