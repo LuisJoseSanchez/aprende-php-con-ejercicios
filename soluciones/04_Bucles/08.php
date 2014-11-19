1738bbd67efa3877e4d78f00cd52322201dee4aa
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-19 20:40:52 +0100

    Revisando las soluciones del capítulo 4.

--- soluciones/04_Bucles/08.php
+++ soluciones/04_Bucles/08.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-    Por favor, introduzca un número del 0 al 10:
-    <form action="08result.php" method="post">
-        <input type="text" name="n"><br>
-        <input type="submit" value="Aceptar">
-    </form>
+        Por favor, introduzca un número del 0 al 10:
+        <form action="08result.php" method="post">
+          <input type="number" name="n" min="0" max="10"><br>
+          <input type="submit" value="Aceptar">
+        </form>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
