83724aadeefb5fff443b49a5018e897aca43b60f
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-19 20:26:48 +0100

    Revisando las soluciones del capítulo 4.

--- soluciones/04_Bucles/07.php
+++ soluciones/04_Bucles/07.php
@@ -18,35 +18,38 @@
         </h2>
       </div>
 
-      <?php
-        if (!isset($_POST['oportunidades'])) {
-          $numeroIntroducido = 55555;
-          $oportunidades = 4;
-        } else {
-          $oportunidades = $_POST['oportunidades'];
-          $numeroIntroducido = $_POST['numeroIntroducido'];
-        }
+      <div id="content">
 
-        $numeroSecreto = 2405;
+        <?php
+          if (!isset($_POST['oportunidades'])) {
+            $numeroIntroducido = 55555;
+            $oportunidades = 4;
+          } else {
+            $oportunidades = $_POST['oportunidades'];
+            $numeroIntroducido = $_POST['numeroIntroducido'];
+          }
 
-        if ($numeroIntroducido == $numeroSecreto) {
-          echo "La caja fuerte se ha abierto.";
-        } else if ($oportunidades == 0) {
-          echo "Lo siento, has agotado todas tus oportunidades. La caja fuerte permanecerá cerrada.";
-        } else {
-          echo "Te quedan ", $oportunidades, " oportunidades para abrir la caja fuerte.<br>";
-          $oportunidades--;
-          echo "Introduce un número de cuatro cifras.";
-          echo '<form action="07.php" method="post">';
-          echo '<input type="number" min=0 max=9999 name="numeroIntroducido" autofocus>';
-          echo '<input type="hidden" name="oportunidades" value="', $oportunidades, '">';
-          echo '<input type="submit" value="Continuar">';
-          echo '</form>';
-        }
-      ?>
-      <br><br>
-      <a href="index.php">>> Volver</a>
-   
+          $numeroSecreto = 2405;
+
+          if ($numeroIntroducido == $numeroSecreto) {
+            echo "La caja fuerte se ha abierto.";
+          } else if ($oportunidades == 0) {
+            echo "Lo siento, has agotado todas tus oportunidades. La caja fuerte permanecerá cerrada.";
+          } else {
+            echo "Te quedan ", $oportunidades, " oportunidades para abrir la caja fuerte.<br>";
+            $oportunidades--;
+            echo "Introduce un número de cuatro cifras.";
+            echo '<form action="07.php" method="post">';
+            echo '<input type="number" min=0 max=9999 name="numeroIntroducido" autofocus>';
+            echo '<input type="hidden" name="oportunidades" value="', $oportunidades, '">';
+            echo '<input type="submit" value="Continuar">';
+            echo '</form>';
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
+      </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
