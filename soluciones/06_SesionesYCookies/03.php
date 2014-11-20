158a9d1f014d8befbd0c13a1fb080fcb54430e22
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-20 11:22:30 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_SesionesYCookies/03.php
+++ soluciones/06_SesionesYCookies/03.php
@@ -1,57 +1,64 @@
-<?php session_start(); ?>
+<?php
+  session_start();
+  
+  if (!isset($_POST['n'])) {
+    $_SESSION['cuentaNumeros'] = 0;
+    $_SESSION['suma'] = 0;
+  }
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>6. Sesiones y Cookies
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>6. Sesiones y cookies
         </h2>
       </div>
 
       <div id="content">
-               Este programa muestra la suma, el contador de los números introducidos y la media.<br>
-               Vaya introduciendo números, el programa parará cuando la suma de ellos supere 10000.<br>
-                <?php
-          $n = $_POST['n'];
+        Este programa muestra la suma, el contador de los números introducidos y la media.<br>
+        Vaya introduciendo números, el programa parará cuando la suma de ellos supere 10000.<br>
+        <?php
+					$n = $_POST['n'];
 
-          if (isset($n)) {
-            $_SESSION['cuentaNumeros']++;
-            $_SESSION['suma'] += $n;
-            
-            if ($_SESSION['suma'] >10000) {
-                echo "<br><br>Ha introducido ".$_SESSION['cuentaNumeros']." números.<br>";
-                echo "La suma es ".$_SESSION['suma']."<br>";
-                echo "La media es ".$_SESSION['suma']/$_SESSION['cuentaNumeros']."<br>";
-            } else {
-                echo "<form action=\"03.php\" method=\"post\">";
-                echo "<input type=\"text\" name=\"n\" autofocus>";
-                echo "<input type=\"submit\" value=\"Aceptar\">";
-                echo "</form>";
-            }
-          } else {
-            $_SESSION['cuentaNumeros'] = 0;
-            $_SESSION['suma'] = 0;
-            echo "<form action=\"03.php\" method=\"post\">";
-            echo "<input type=\"text\" name=\"n\" autofocus>";
-            echo "<input type=\"submit\" value=\"Aceptar\">";
-            echo "</form>";
+          if (!isset($n) || ($_SESSION['suma'] <= 10000)) {
+          ?>
+            <form action="03.php" method="post">
+              <input type="number" name="n" autofocus>
+              <input type="submit" value="Aceptar">
+            </form>
+          <?php
           }
-        ?>
-      </div>
-      <div id="footer">
-        © Luis José Sánchez González
-      </div>
-  </div>
-    </body>
+          
+					if (isset($n)) {
+						$_SESSION['cuentaNumeros']++;
+						$_SESSION['suma'] += $n;
+						
+            if ($_SESSION['suma'] > 10000) {
+								echo "<br><br>Ha introducido ".$_SESSION['cuentaNumeros']." números.<br>";
+								echo "La suma es ".$_SESSION['suma']."<br>";
+								echo "La media es ".$_SESSION['suma']/$_SESSION['cuentaNumeros']."<br>";
+						}
+					}
+				?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
+	    </div>
+      
+	    <div id="footer">
+		    © Luis José Sánchez González
+	    </div>
+    </div>
+  </body>
 </html>
