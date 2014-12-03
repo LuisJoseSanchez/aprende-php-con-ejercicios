7a743cc42be411d9172957ab6132e040dd89970d
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-03 09:27:38 +0100

    Revisando las soluciones del capítulo 4 (bucles).

--- soluciones/04_Bucles/15.php
+++ soluciones/04_Bucles/15.php
@@ -1,52 +1,56 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-        $base = $_POST['base'];
-        $exponenteFinal = $_POST['exponenteFinal'];
-        
-                    if (!isset($base)) {
-      echo "Este programa calcula una serie de potencias sucesivas a partir de la base y el exponente.<br>";
-      echo "<form action=\"15.php\" method=\"post\">";
-                        echo "Base: <input type=\"text\" name=\"base\" size=\"5\"><br>";
-      echo "Exponente: <input type=\"text\" name=\"exponenteFinal\" size=\"5\"><br>";
-                        echo "<input type=\"submit\" value=\"Aceptar\">";
-                        echo "</form>";
-                    } else {
+        <?php
+          $base = $_POST['base'];
+          $exponenteFinal = $_POST['exponenteFinal'];
+
+          if (!isset($base)) {
+          ?>
+            Este programa calcula una serie de potencias sucesivas a partir de la base y el exponente.<br>
+            <form action="15.php" method="post">
+              Base: <input type="number" name="base" autofocus><br>
+              Exponente: <input type="number" name="exponenteFinal"><br>
+              <input type="submit" value="Aceptar">
+            </form>
+          <?php
+          } else {
             for ($i = 1; $i <= $exponenteFinal; $i++) {
-          $potencia = 1;
-          $exponente = $i;
-        
-          for ($j = 0; $j < $exponente; $j++)
-            $potencia *= $base;
+              $potencia = 1;
+              $exponente = $i;
 
-          echo "$base<sup>$exponente</sup> = $potencia<br>";
-      }
-        }
-    ?>
+              for ($j = 0; $j < $exponente; $j++) {
+                $potencia *= $base;
+              }
 
+              echo "$base<sup>$exponente</sup> = $potencia<br>";
+            }
+          }
+          ?>
+          <br><br>
+          <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
