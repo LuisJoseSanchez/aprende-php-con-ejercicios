db47c4ab888d8c322c705771b3943121411b7e84
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-03 10:18:19 +0100

    Revisando las soluciones del capítulo 4 (bucles).

--- soluciones/04_Bucles/16.php
+++ soluciones/04_Bucles/16.php
@@ -1,51 +1,62 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-        $n = $_POST['n'];
+        <?php
+          $n = $_POST['n'];
         
-                    if (!isset($n)) {
-      echo "Introduzca un número para saber si es primo o no.<br>";
-      echo "<form action=\"16.php\" method=\"post\">";
-                        echo "<input type=\"text\" name=\"n\" size=\"8\"><br>";
-                        echo "<input type=\"submit\" value=\"Aceptar\">";
-                        echo "</form>";
-                    } else {
-      $esPrimo = true;
+          if (!isset($n)) {
+          ?>
+            Introduzca un número para saber si es primo o no.<br>
+            <form action=16.php method="post">
+              <input type="number" name="n" min="0" autofocus><br>
+              <input type="submit" value="Aceptar">
+            </form>
+          <?php
+          } else {
+            $esPrimo = true;
       
-            for ($i = 2; $i < $n; $i++)
-          if ($n % $i == 0)
-        $esPrimo = false;
+            for ($i = 2; $i < $n; $i++) {
+              if ($n % $i == 0) {
+                $esPrimo = false;
+              }
+            }
           
-      if ($esPrimo)
-          echo "El $n es primo.";
-      else
-          echo "El $n no es primo.";
-        }
-    ?>
-
+            // El 0 y el 1 no se consideran primos
+            if (($n == 0) || ($n == 1)) {
+              $esPrimo = false;
+            }
+            
+            if ($esPrimo) {
+                echo "El $n es primo.";
+            } else {
+                echo "El $n no es primo.";
+            }
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
--- soluciones/04_Bucles/18.php
+++ soluciones/04_Bucles/18.php
@@ -1,51 +1,55 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-        $n1 = $_POST['n1'];
-        $n2 = $_POST['n2'];
+        <?php
+          $n1 = $_POST['n1'];
+          $n2 = $_POST['n2'];
         
-                    if ((!isset($n1)) || ($n1 == $n2)){
-      echo "Por favor, introduzca dos números enteros que no sean iguales.<br>";
-      echo "<form action=\"18.php\" method=\"post\">";
-                        echo "<input type=\"text\" name=\"n1\" size=\"8\"><br>";
-      echo "<input type=\"text\" name=\"n2\" size=\"8\"><br>";
-                        echo "<input type=\"submit\" value=\"Aceptar\">";
-                        echo "</form>";
-                    } else {
-      if ($n1 > $n2) {
-          $aux = $n1;
-          $n1 = $n2;
-          $n2 = $aux;
-      }
+          if ((!isset($n1)) || ($n1 == $n2)){
+          ?>
+            Por favor, introduzca dos números enteros que no sean iguales.<br>
+            <form action="18.php" method="post">
+              <input type="number" name="n1" autofocus=""><br>
+              <input type="number" name="n2"><br>
+              <input type="submit" value="Aceptar">
+            </form>
+          <?php
+          } else {
+            if ($n1 > $n2) {
+                $aux = $n1;
+                $n1 = $n2;
+                $n2 = $aux;
+            }
       
-            for ($i = $n1; $i <= $n2; $i+=7)
-          echo "$i  ";          
-        }
-    ?>
-
+            for ($i = $n1; $i <= $n2; $i+=7) {
+              echo "$i  ";
+            }
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
