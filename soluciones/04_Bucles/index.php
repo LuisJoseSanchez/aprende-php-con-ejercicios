ca0d9148d626d7fb7f3e9f955d625724fd718cb5
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-09 11:27:17 +0100

    Revisando las soluciones del capítulo 4 (bucles).

--- soluciones/04_Bucles/index.php
+++ soluciones/04_Bucles/index.php
@@ -157,7 +157,7 @@
           <li>
             Realiza un programa que vaya pidiendo números hasta que se introduzca
             un numero negativo y nos diga cuantos números se han introducido, la
-            media de los impares y el mayor de los pares . El número negativo sólo
+            media de los impares y el mayor de los pares. El número negativo sólo
             se utiliza para indicar el final de la introducción de datos pero no se
             incluye en el cómputo.
             <br /><a href="21.php">&gt;&gt; Solución</a>
