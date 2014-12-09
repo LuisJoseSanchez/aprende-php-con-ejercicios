004a999412e1adad0a48fabd061859adae38b906
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-09 11:52:58 +0100

    Revisando las soluciones del capítulo 4 (bucles).

--- soluciones/04_Bucles/22.php
+++ soluciones/04_Bucles/22.php
@@ -1,41 +1,45 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-        echo "Números primos entre 2 y 100: <br>";
+        <?php
+          echo "Números primos entre 2 y 100: <br>";
 
-        for ($n = 2; $n <= 100; $n++) {
-      $esPrimo = true;
-      for ($i = 2; $i < $n; $i++)
-        if ($n%$i == 0) $esPrimo = false;
-
-      if ($esPrimo)  echo "$n&nbsp;&nbsp;&nbsp;";
-        }
-
-    ?>
-
+          for ($n = 2; $n <= 100; $n++) {
+            $esPrimo = true;
+            for ($i = 2; $i < $n; $i++) {
+              if ($n % $i == 0) {
+                $esPrimo = false;
+              }
+            }
+            if ($esPrimo) {
+              echo "$n&nbsp;&nbsp;&nbsp;";
+            }
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
