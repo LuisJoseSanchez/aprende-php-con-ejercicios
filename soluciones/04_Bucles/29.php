4d925a1478c9011a48b42f70563a8234f7147bf7
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-11 12:23:16 +0100

    Revisados los ejercicios del capítulo 4 - Bucles.

--- soluciones/04_Bucles/29.php
+++ soluciones/04_Bucles/29.php
@@ -1,49 +1,54 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
-        $numeroGrande = $_POST['numeroGrande'];
-        $numeroPequeno = $_POST['numeroPequeno'];
+        <?php
+          $numeroGrande = $_POST['numeroGrande'];
+          $numeroPequeno = $_POST['numeroPequeno'];
         
-                    if (!isset($numeroGrande)){
-      echo "<form action=\"29.php\" method=\"post\">";
-      echo "Introduzca un número entero positivo (relativamente grande): ";
-                        echo "<input type=\"text\" name=\"numeroGrande\" size=\"8\"><br>";
-      echo "Introduzca otro número (relativamente pequeño): ";
-                        echo "<input type=\"text\" name=\"numeroPequeno\" size=\"4\"><br>";
-                        echo "<input type=\"submit\" value=\"Aceptar\">";
-                        echo "</form>";
-                    } else {
-      $cuenta = 0;
-      $suma = 0;
-      
-      echo "Los números enteros positivos menores que $numeroGrande";
-      echo " que no son divisibles entre $numeroPequeno son los siguientes: <br>";
+          if (!isset($numeroGrande)){
+          ?>
+            <form action="29.php" method="post">
+              Introduzca un número entero positivo (relativamente grande):
+              <input type="number" name="numeroGrande" min="0" autofocus="" required=""><br>
+              Introduzca otro número (relativamente pequeño):
+              <input type="number" name="numeroPequeno" min="0" required=""><br>
+              <input type="submit" value="Aceptar">
+            </form>
+        <?php
+          } else {
+            $cuenta = 0;
+            $suma = 0;
 
-      for ($i = 1; $i < $numeroGrande; $i++)
-        if (($i % $numeroPequeno) != 0)
-          echo "$i ";
-        }
-    ?>
+            echo "Los números enteros positivos menores que $numeroGrande";
+            echo " que no son divisibles entre $numeroPequeno son los siguientes: <br>";
 
+            for ($i = 1; $i < $numeroGrande; $i++) {
+              if (($i % $numeroPequeno) != 0) {
+                echo "$i ";
+              }
+            }
+          }
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
       <div id="footer">
         © Luis José Sánchez González
