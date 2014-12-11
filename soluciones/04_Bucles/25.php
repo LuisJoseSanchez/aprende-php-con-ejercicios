337809692f53aabca84d373e38ff4b6783a76858
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-11 10:29:13 +0100

    Revisando ejercicios del capítulo 4 - Bucles.

--- soluciones/04_Bucles/25.php
+++ soluciones/04_Bucles/25.php
@@ -1,50 +1,54 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-  <div id="container">
+  <head>
+    <meta charset="UTF-8">
+    <link href="default.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
       <div id="header">
         <h1>
           APRENDE PHP CON EJERCICIOS
         </h1>
-                    <h2>
-                            SOLUCIONES A LOS EJERCICIOS
-                    </h2>
         <h2>
-                        <br>4. Bucles
+          SOLUCIONES A LOS EJERCICIOS
+        </h2>
+        <h2>
+          <br>4. Bucles
         </h2>
       </div>
 
       <div id="content">
-                <?php
+      <?php
         $n = $_POST['n'];
         
-                    if (!isset($n)){
-      echo "Por favor, introduzca un número entero:";
-      echo "<form action=\"25.php\" method=\"post\">";
-                        echo "<input type=\"text\" name=\"n\" size=\"8\"><br>";
-                        echo "<input type=\"submit\" value=\"Aceptar\">";
-                        echo "</form>";
-                    } else {
-      $volteado = 0;
+        if (!isset($n)){
+        ?>
+        Por favor, introduzca un número entero:
+        <form action="25.php" method="post">
+          <input type="number" name="n" min="0" autofocus="" required=""><br>
+          <input type="submit" value="Aceptar">
+        </form>
+        <?php
+        } else {
+          $volteado = 0;
 
-      $numero = $n;
-      while ($numero > 0) {
-        $volteado = ($volteado * 10) + ($numero % 10);
-        $numero = floor($numero/10);
-      } // while
+          $numero = $n;
+          while ($numero > 0) {
+            $volteado = ($volteado * 10) + ($numero % 10);
+            $numero = floor($numero / 10);
+          } // while
 
-      echo "Si le damos la vuelta al $n tenemos el $volteado";
+          echo "Si le damos la vuelta al $n tenemos el $volteado";
         }
-    ?>
-
+        ?>
+        <br><br>
+        <a href="index.php">>> Volver</a>
       </div>
+      
       <div id="footer">
         © Luis José Sánchez González
       </div>
-  </div>
-    </body>
+    </div>
+  </body>
 </html>
