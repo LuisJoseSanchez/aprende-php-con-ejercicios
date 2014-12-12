8a9e0b44afc8f7768373dba2a4f13fa94ad68562
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-12 11:17:11 +0100

    Revisando ejercicios del capítulo 5 - Arrays.

--- soluciones/05_Arrays/02.php
+++ soluciones/05_Arrays/02.php
@@ -1,55 +1,67 @@
 <?php
+
   if (!isset($_GET['n'])) {
-       $contadorNumeros = 1;
-       $numeroTexto = "";
-       echo '<form action="02.php" method="get">';
-       echo 'Introduzca un número:';
-       echo '<input type="text" name ="n">';
-       echo '<input type="hidden" name="contadorNumeros" value="'.$contadorNumeros.'">';
-       echo '<input type="submit" value="OK">';
-       echo '</form>';
-   } else {
-         $contadorNumeros = $_GET['contadorNumeros'];
-         $n = $_GET['n'];
-         $numeroTexto = $_GET['numeroTexto'];
+    $contadorNumeros = 1;
+    $numeroTexto = "";
+  } else {
+    $contadorNumeros = $_GET['contadorNumeros'];
+    $numeroTexto = $_GET['numeroTexto'];
+  }
+  
+  if ($contadorNumeros < 10) {
+    $contadorNumeros = $_GET['contadorNumeros'];
+    $n = $_GET['n'];
+    $numeroTexto = $_GET['numeroTexto'];
 
-         if ($numeroTexto=="")
-             $numeroTexto = $n;
-         else
-             $numeroTexto = $numeroTexto.' '.$n;
+    if ($numeroTexto == "") {
+        $numeroTexto = $n;
+    } else {
+        $numeroTexto = $numeroTexto.' '.$n;
+    }
+    
+    $contadorNumeros++;
+  }
+  
+  if (!isset($_GET['n']) || ($contadorNumeros < 10)) {
+  ?>
+    <form action="pagina.php" method="get">
+      <input type="hidden" name="ejercicio" value="02">
+      Introduzca un número:
+      <input type="number" name ="n" autofocus="" required="">
+      <input type="hidden" name="contadorNumeros" value="<?php echo $contadorNumeros; ?>">
+      <input type="hidden" name="numeroTexto" value="<?php echo $numeroTexto; ?>">
+      <input type="submit" value="OK">
+    </form>
+  <?php
+  }
+  
+  ////////////////////////////////////////////////////////////////
+  //  Programa principal una vez recogidos los datos del array
+  //  El array con los números es $numero
+  ////////////////////////////////////////////////////////////////
 
-         if ($contadorNumeros < 10) {
-             $contadorNumeros++;
-             echo '<form action="02.php" method="get">';
-             echo 'Introduzca un número:';
-             echo '<input type="text" name="n">';
-             echo '<input type="hidden" name="contadorNumeros" value="'.$contadorNumeros.'">';
-             echo '<input type="hidden" name="numeroTexto" value="'.$numeroTexto.'">';
-             echo '<input type="submit" value="OK">';
-             echo '</form>';
-         } else {
-             ////////////////////////////////////////////////////////////////
-             //  Programa principal una vez recogidos los datos del array
-             //  El array con los números es $numero
-             ////////////////////////////////////////////////////////////////
+  if ($contadorNumeros == 10) {
+    $numero = explode(" ", $numeroTexto);
 
-             $numero = explode(" ", $numeroTexto);
+    $maximo = -PHP_INT_MAX;
+    $minimo = PHP_INT_MAX;
 
-             $maximo = -PHP_INT_MAX;
-             $minimo = PHP_INT_MAX;
+    foreach ($numero as $n) {
+      if ($n < $minimo) {
+        $minimo = $n;
+      }
+      if ($n > $maximo) {
+        $maximo = $n;
+      }
+    }
 
-             foreach ($numero as $n) {
-                 if ($n < $minimo)  $minimo = $n;
-                 if ($n > $maximo)  $maximo = $n;
-             }
-
-             foreach ($numero as $n) {
-                 if ($n == $minimo)
-                        echo "$n mínimo<br>";
-                 else if ($n == $maximo)
-                        echo "$n máximo<br>";
-                 else
-                     echo "$n<br>";
-             }
-         }
-     }
+    foreach ($numero as $n) {
+      if ($n == $minimo) {
+        echo "$n mínimo<br>";
+      } else if ($n == $maximo) {
+        echo "$n máximo<br>";
+      } else {
+        echo "$n<br>";
+      }
+    }
+  }
