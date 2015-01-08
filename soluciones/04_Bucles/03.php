15cd58f410e69e4ec3a808afafa83e8beeaedfe1
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-08 11:02:17 +0100

    Revisando ejercicios del capítulo 5 - Arrays

--- soluciones/04_Bucles/03.php
+++ soluciones/04_Bucles/03.php
@@ -1,83 +1,91 @@
 <?php
-    if (!isset($_GET['n'])) {
-         $contadorNumeros = 1;
-         $numeroTexto = "";
-         echo '<form action="03.php" method="get">';
-         echo 'Introduzca un número:';
-         echo '<input type="text" name ="n">';
-         echo '<input type="hidden" name="contadorNumeros" value="'.$contadorNumeros.'">';
-         echo '<input type="submit" value="OK">';
-         echo '</form>';
-     } else {
-         $contadorNumeros = $_GET['contadorNumeros'];
-         $n = $_GET['n'];
-         $numeroTexto = $_GET['numeroTexto'];
 
-         if ($numeroTexto=="")
-             $numeroTexto = $n;
-         else
-             $numeroTexto = $numeroTexto.' '.$n;
+  if (!isset($_GET['n'])) {
+    $contadorNumeros = 1;
+    $numeroTexto = "";
+  } else {
+    $contadorNumeros = $_GET['contadorNumeros'];
+    $numeroTexto = $_GET['numeroTexto'];
+  }
+  
+  if ($contadorNumeros < 16) {
+    $contadorNumeros = $_GET['contadorNumeros'];
+    $n = $_GET['n'];
+    $numeroTexto = $_GET['numeroTexto'];
 
-         if ($contadorNumeros < 15) {
-             $contadorNumeros++;
-             echo '<form action="03.php" method="get">';
-             echo 'Introduzca un número:';
-             echo '<input type="text" name="n">';
-             echo '<input type="hidden" name="contadorNumeros" value="'.$contadorNumeros.'">';
-             echo '<input type="hidden" name="numeroTexto" value="'.$numeroTexto.'">';
-             echo '<input type="submit" value="OK">';
-             echo '</form>';
-         } else {
-             ////////////////////////////////////////////////////////////////
-             //  Programa principal una vez recogidos los datos del array
-             //  El array con los números es $numero
-             ////////////////////////////////////////////////////////////////
+    if ($numeroTexto == "") {
+        $numeroTexto = $n;
+    } else {
+        $numeroTexto = $numeroTexto.' '.$n;
+    }
+    
+    $contadorNumeros++;
+  }
+  
+  if (!isset($_GET['n']) || ($contadorNumeros < 16)) {
+  ?>
+    <form action="pagina.php" method="get">
+      <input type="hidden" name="ejercicio" value="03">
+      Introduzca un número:
+      <input type="number" name ="n" autofocus="" required="">
+      <input type="hidden" name="contadorNumeros" value="<?php echo $contadorNumeros; ?>">
+      <input type="hidden" name="numeroTexto" value="<?php echo $numeroTexto; ?>">
+      <input type="submit" value="OK">
+    </form>
+  <?php
+  }
 
-             $numero = explode(" ", $numeroTexto);
+  ////////////////////////////////////////////////////////////////
+  //  Programa principal una vez recogidos los datos del array
+  //  El array con los números es $numero
+  ////////////////////////////////////////////////////////////////
 
+  if ($contadorNumeros == 16) {
+    $numero = explode(" ", $numeroTexto);
 
-             // Muestra el array original
+    // Muestra el array original
+    echo "Array original:<br>";
+    echo "<table><tr>";
 
-             echo "Array original:<br>";
+    for ($i = 0; $i < 15; $i++) {
+      echo "<td>$i</td>";
+    }
 
-             echo "<table><tr>";
+    echo "</tr>";
 
-             for ($i = 0; $i < 15; $i++)
-                echo "<td>$i</td>";
+    for ($i = 0; $i < 15; $i++) {
+       echo "<td>".$numero[$i]."</td>";
+    }
 
-             echo "</tr>";
-
-             for ($i = 0; $i < 15; $i++)
-                echo "<td>".$numero[$i]."</td>";
-
-             echo "</tr></table>";
-
-
-             // Rota los elementos del array hacia la derecha
-
-             $aux = $numero[14];
-
-             for ($i = 14; $i > 0; $i--)
-                $numero[$i] = $numero[$i - 1];
-
-             $numero[0] = $aux;
+    echo "</tr></table>";
 
 
-             // Muestra el array rotado
+    // Rota los elementos del array hacia la derecha
 
-             echo "<br>Array rotado:<br>";
+    $aux = $numero[14];
 
-             echo "<table><tr>";
+    for ($i = 14; $i > 0; $i--) {
+      $numero[$i] = $numero[$i - 1];
+    }
 
-             for ($i = 0; $i < 15; $i++)
-                echo "<td>$i</td>";
+    $numero[0] = $aux;
 
-             echo "</tr>";
 
-             for ($i = 0; $i < 15; $i++)
-                echo "<td>".$numero[$i]."</td>";
+    // Muestra el array rotado
 
-             echo "</tr></table>";
-         }
-     }
+    echo "<br>Array rotado:<br>";
+    echo "<table><tr>";
+
+    for ($i = 0; $i < 15; $i++) {
+      echo "<td>$i</td>";
+    }
+
+    echo "</tr>";
+
+    for ($i = 0; $i < 15; $i++) {
+      echo "<td>".$numero[$i]."</td>";
+    }
+
+    echo "</tr></table>";
+  }
 ?>
