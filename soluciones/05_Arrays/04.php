291dff3a2014955e28386ec691606bd100e2898d
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-08 12:43:46 +0100

    Revisando ejercicios del capítulo 5 - Arrays

--- soluciones/05_Arrays/04.php
+++ soluciones/05_Arrays/04.php
@@ -1,32 +1,34 @@
-                <?php
-                    if (!isset($_GET['numeroTexto'])) {
-                        // Genera el array aleatorio
-                        for ($i = 0; $i < 100; $i++) {
-                            $numero[$i] = rand(0, 20);
-                            echo $numero[$i]." ";
-                        }
-                        
-                        $numeroTexto = implode(" ", $numero);
-                        
-                        echo '<form action="04.php" method="get">';
-                        echo 'Valor a sustituir: ';
-                        echo '<input type="text" name ="viejo">';
-                        echo 'Valor nuevo: ';
-                        echo '<input type="text" name ="nuevo">';
-                        echo '<input type="hidden" name="numeroTexto" value="'.$numeroTexto.'">';
-                        echo '<input type="submit" value="OK">';
-                        echo '</form>';
-                     } else {
-                        $numeroTexto = $_GET['numeroTexto'];
-                        $viejo = $_GET['viejo'];
-                        $nuevo = $_GET['nuevo'];
-                        
-                        $numero = explode(" ", $numeroTexto);
-                        
-                        foreach ($numero as $n) {
-                            if ($n == $viejo) 
-                                    echo "<span style=\"color: green; font-weight: bold;\">$nuevo</span> ";
-                                else
-                                    echo  "$n ";
-                         }
-                     }
+<?php
+  if (!isset($_GET['numeroTexto'])) {
+    // Genera el array aleatorio
+    for ($i = 0; $i < 100; $i++) {
+        $numero[$i] = rand(0, 20);
+        echo $numero[$i]." ";
+    }
+
+    $numeroTexto = implode(" ", $numero);
+    ?>
+    <br><br>
+    <form action="pagina.php" method="get">
+      <input type="hidden" name="ejercicio" value="04">
+      Valor a sustituir: <input type="number" name ="viejo" autofocus="" required=""><br>
+      Valor nuevo: <input type="number" name ="nuevo" required="">
+      <input type="hidden" name="numeroTexto" value="<?php echo $numeroTexto; ?>">
+      <input type="submit" value="OK">
+    </form>
+    <?php
+  } else {
+    $numeroTexto = $_GET['numeroTexto'];
+    $viejo = $_GET['viejo'];
+    $nuevo = $_GET['nuevo'];
+
+    $numero = explode(" ", $numeroTexto);
+
+    foreach ($numero as $n) {
+      if ($n == $viejo) { 
+        echo "<span style=\"color: green; font-weight: bold;\">$nuevo</span> ";
+      } else {
+        echo  "$n ";
+      }
+    }
+  }
