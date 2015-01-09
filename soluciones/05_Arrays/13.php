bf7f034af38bcc614d870627636740674ffcc89d
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-09 18:32:52 +0100

    Revisados los ejercicios del capítulo 5 - Arrays

--- soluciones/05_Arrays/13.php
+++ soluciones/05_Arrays/13.php
@@ -1,44 +1,47 @@
+<h2>Resalta el mínimo y sus diagonales</h2>
+<?php
+  // Genera un array con números aleatorios que no se repiten
+  // Primero se meten en un array lineal (todos seguidos)...
+  $i = 0;
+  do {
+    $n = rand(100, 999);
+    if (!in_array($n, $lineal)) {
+      $lineal[] = $n;
+      $i++;
+    }
+  } while ($i < 54);
+  // ...y después se vuelca en un array de 9x6
+  // al mismo tiempo que se calculan las coordenadas
+  // del mínimo
+  $minimo = 999;
+  $i = 0;
+  for ($x = 0; $x < 9; $x++) {
+    for ($y = 0; $y < 6; $y++) {
+      $numero[$x][$y] = $lineal[$i];
+      $i++;              
+      if ($numero[$x][$y] < $minimo) {
+        $minimo = $numero[$x][$y];
+        $xMinimo = $x;
+        $yMinimo = $y;
+      }
+    }
+  }
 
-                <h2>Resalta el mínimo y sus diagonales</h2>
-                
-                <?php
-        
-          // Genera un array con números aleatorios que no se repiten
-          // Primero se meten en un array lineal (todo seguido)...
-          $i = 0;
-          do {
-            $n = rand(100, 999);
-            if (!in_array($n, $lineal)) {
-              $lineal[] = $n;
-              $i++;
-            }
-          } while ($i < 54);
-          // ...y después de vuelca en un array de 9x6
-          // al mismo tiempo que se calculan las coordenadas
-          // del mínimo
-          $minimo = 999;
-          $i = 0;
-          for ($x = 0; $x < 9; $x++)
-            for ($y = 0; $y < 6; $y++) {
-              $numero[$x][$y] = $lineal[$i];
-              $i++;              
-              if ($numero[$x][$y] < $minimo) {
-                $minimo = $numero[$x][$y];
-                $xMinimo = $x;
-                $yMinimo = $y;
-              }
-            }
-            
-          // Se muestra el array con el mínimo en azul y sus
-          // diagonales en verde
-          // Nota: abs($x) devuelve el valor absoluto de $x
-          for ($x = 0; $x < 9; $x++) {
-            for ($y = 0; $y < 6; $y++)
-              if ($numero[$x][$y] == $minimo)
-                echo '<span style="color: blue; font-weight:bold">'.$numero[$x][$y].' </span>';
-              else if (abs((abs($x) - abs($xMinimo))) == abs((abs($y) - abs($yMinimo))))
-                echo '<span style="color: green; font-weight:bold">'.$numero[$x][$y].' </span>';
-              else
-                echo $numero[$x][$y]." ";
-            echo "<br>";  
-          }            
+  // Se muestra el array con el mínimo en azul y sus
+  // diagonales en verde
+  // Nota: abs($x) devuelve el valor absoluto de $x
+  echo "<table>";
+  for ($x = 0; $x < 9; $x++) {
+    echo "<tr>";
+    for ($y = 0; $y < 6; $y++) {
+      if ($numero[$x][$y] == $minimo) {
+        echo '<td><span style="color: blue; font-weight:bold">'.$numero[$x][$y].' </span></td>';
+      } else if (abs((abs($x) - abs($xMinimo))) == abs((abs($y) - abs($yMinimo)))) {
+        echo '<td><span style="color: green; font-weight:bold">'.$numero[$x][$y].' </span></td>';
+      } else {
+        echo '<td>'.$numero[$x][$y].'</td>';
+      }
+    }
+    echo "</tr>";  
+  }
+  echo "</table>";
--- soluciones/05_Arrays/14.php
+++ soluciones/05_Arrays/14.php
@@ -1,59 +1,72 @@
-  <style type="text/css">
-    table, th, td {
-      border: 0px;
-      text-align: center;
-    }
-    td.black {
-      background-color: black;
-    }
-    td.white {
-      background-color: white;
-    }
-  </style>
+<style type="text/css">
+  table, th, td {
+    border: 0px;
+    text-align: center;
+  }
+  td.negro {
+    background-color: black;
+  }
+  td.blanco {
+    background-color: white;
+  }
+  tr.marron {
+    background-color: brown;
+  }
+</style>
 
-                <h2> Movimiento de un alfil</h2>
-                
-                <?php
-          // Recoge la posición del alfil
-          $posicion = $_POST['posicion'];
-          $x = ord(substr($posicion, 0, 1)) - ord('a');
-          $y = 8 - substr($posicion, 1, 1);
-          
-          // Pinta el tablero de ajedrez
-          $color = "white"; // color de la primera casilla
-          echo "<table><td></td><td>a</td><td>b</td><td>c</td><td>d</td><td>e</td><td>f</td><td>g</td><td>h</td>";
-          for ($fila = 0; $fila < 8; $fila++) {
-            echo "<tr><td style=\"text-align: right\">".(8 - $fila)."</td>";
-            for ($columna = 0; $columna < 8; $columna++) {
-              echo "<td class=\"$color\">";
-              
-              // Comprueba si el alfil está en la posición actual
-              if (($x == $columna) && ($y == $fila))
-                echo "<img src=\"alfil.png\">";
-              // Comprueba si es una posición a la que puede llegar el alfil
-              else if (abs((abs($x) - abs($columna))) == abs((abs($y) - abs($fila))))
-                echo "<img src=\"alfilsemitransparente.png\">";
-              else
-                echo "<img src=\"vacio.png\">";
-              echo "</td>";
-              
-              // Alterna el color de la casilla
-              if ($color == "white")
-                  $color = "black";
-                else
-                  $color = "white";
-              echo "</td>";
-            }
-            if ($color == "white")
-                  $color = "black";
-                else
-                  $color = "white";
-            echo "<td style=\"text-align: left\">".(8 - $fila)."</td></tr>";
-          }
-          echo "<td></td><td>a</td><td>b</td><td>c</td><td>d</td><td>e</td><td>f</td><td>g</td><td>h</td></table>";
+<h2> Movimiento de un alfil</h2>
+<?php
+  // Recoge la posición del alfil
+  $posicion = $_GET['posicion'];
+  $x = ord(substr($posicion, 0, 1)) - ord('a');
+  $y = 8 - substr($posicion, 1, 1);
 
-          echo 'Introduzca las coordenadas del alfil (p. ej. e4)';
-          echo '<form action="14.php" method="post">';
-          echo '<input type="text" name="posicion"><br>';
-          echo '<input type="submit" value="Aceptar">';
-          echo '</form>';
+  // Pinta el tablero de ajedrez
+  $color = "blanco"; // color de la primera casilla
+  echo '<table><tr class="marron">';
+  echo '<td></td><td>a</td><td>b</td><td>c</td><td>d</td><td>e</td><td>f</td><td>g</td><td>h</td><td></td></tr>';
+  for ($fila = 0; $fila < 8; $fila++) {
+    echo '<tr><td style="text-align: right; background-color: brown;">'.(8 - $fila).'</td>';
+    for ($columna = 0; $columna < 8; $columna++) {
+      echo "<td class=\"$color\">";
+
+      // Comprueba si el alfil está en la posición actual
+      if (($x == $columna) && ($y == $fila)) {
+        echo '<img src="alfil.png">';
+      // Comprueba si es una posición a la que puede llegar el alfil
+      } else if (abs((abs($x) - abs($columna))) == abs((abs($y) - abs($fila)))) {
+        echo '<img src="alfilsemitransparente.png">';
+      } else {
+        echo '<img src="vacio.png">';
+      }
+      echo "</td>";
+
+      // Alterna el color de la casilla
+      if ($color == "blanco") {
+        $color = "negro";
+      } else {
+        $color = "blanco";
+      }
+      echo "</td>";
+    }
+    if ($color == "blanco") {
+      $color = "negro";
+    } else {
+      $color = "blanco";
+    }
+    echo '<td style="text-align: left; background-color: brown;">'.(8 - $fila).'</td></tr>';
+  }
+  ?>
+  <tr class="marron">
+    <td></td><td>a</td><td>b</td><td>c</td><td>d</td><td>e</td><td>f</td><td>g</td><td>h</td><td></td>
+  </tr>
+  </table>
+
+  <br>
+  Introduzca las coordenadas del alfil (p. ej. e4)
+  <br>
+  <form action="pagina.php" method="get">
+    <input type="hidden" name="ejercicio" value="14">
+    <input type="text" name="posicion" autofocus="" required=""><br>
+    <input type="submit" value="Aceptar">
+  </form>
