25addaaa601fd13fac50cb7f965463318a7f746c
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-09 09:04:20 +0100

    Revisando ejercicios del capítulo 5 - Arrays

--- soluciones/05_Arrays/05.php
+++ soluciones/05_Arrays/05.php
@@ -1,21 +1,32 @@
-                <?php
-                    if (!isset($_POST['temperatura'])) {
-                         $mes = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-       echo 'Por favor, introduzca la temperatura media de cada mes.<br>';
-       echo '<form action='.$_SERVER['SCRIPT_NAME'].' method="post">';
-       for ($i = 0; $i < 12; $i++)
-          echo "$mes[$i]: <input type=\"text\" name =\"temperatura[$mes[$i]]\" size=\"4\"><br>"; 
-                         echo '<input type="submit" value="OK">';
-                         echo '</form>';
-                     } else {
-                         $temperatura = $_POST['temperatura'];
-       echo "<table>";
-       foreach($temperatura as $mes => $temperaturaMes) {
-           echo "<tr><td>$mes </td><td>";
-           // Pinta la barra
-           for ($i = 0; $i < $temperaturaMes; $i++)
+<?php
+  if (!isset($_GET['temperatura'])) {
+    // Pide los datos de las temperaturas
+    $mes = array(
+      "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+      "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+    ?>
+    Por favor, introduzca la temperatura media de cada mes.<br>
+    <form action="pagina.php" method="get">
+      <input type="hidden" name="ejercicio" value="05">
+      <?php
+      for ($i = 0; $i < 12; $i++) {
+        echo "$mes[$i]: <input type=\"number\" name =\"temperatura[$mes[$i]]\"><br>";
+      }
+      ?>
+      <input type="submit" value="OK">
+    </form>
+    <?php                       
+  } else {
+    // Pinta el diagrama de barras
+    $temperatura = $_GET['temperatura'];
+    echo "<table>";
+    foreach($temperatura as $mes => $temperaturaMes) {
+      echo "<tr><td>$mes </td><td>";
+      // Pinta la barra
+      for ($i = 0; $i < $temperaturaMes; $i++) {
         echo "<img src=\"bggreen.png\">";
-           echo " $temperaturaMes"."ºC<br></td></tr>";
-       }
-       echo "</table>";
-                     }
+      }
+      echo " $temperaturaMes"."ºC<br></td></tr>";
+    }
+    echo "</table>";
+  }
