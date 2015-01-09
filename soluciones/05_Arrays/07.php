95dd13553fa0d47f4bfd50c895248b11b7e5d9fc
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-09 09:35:09 +0100

    Revisando ejercicios del capítulo 5 - Arrays

--- soluciones/05_Arrays/07.php
+++ soluciones/05_Arrays/07.php
@@ -1,58 +1,62 @@
+<?php
+  // Genera el array aleatorio
+  for ($i = 0; $i < 20; $i++) {
+    $numero[$i] = rand(0, 100);
+  }
 
-                <?php
-                    // Genera el array aleatorio
-                    for ($i = 0; $i < 20; $i++)
-                        $numero[$i] = rand(0, 100);
+  // Muestra el array original
 
-                    // Muestra el array original
-                    
-                    // Índice
-                    echo "Array original:<br>";
-                    echo "<table><tr>";
-                    for ($i = 0; $i < 20; $i++)
-                       echo "<td>$i</td>";
-                    echo "</tr>";
-                    
-                    // Contenido
-                    for ($i = 0; $i < 20; $i++)
-                       echo "<td>".$numero[$i]."</td>";
-                    echo "</tr></table>";
+  // Índice
+  echo "Array original:<br>";
+  echo "<table><tr>";
+  for ($i = 0; $i < 20; $i++) {
+    echo "<td>$i</td>";
+  }
+  echo "</tr>";
 
+  // Contenido
+  for ($i = 0; $i < 20; $i++) {
+    echo "<td>".$numero[$i]."</td>";
+  }
+  echo "</tr></table>";
 
-                    // Guarda los pares y los impares en arrays diferentes
-                    $cuentaPares = 0;
-                    $cuentaImpares = 0;
-                    
-                    for ($i = 0; $i < 20; $i++)
-                       if ($numero[$i] % 2 == 0) {
-                           $pares[$cuentaPares] = $numero[$i];
-                           $cuentaPares++;
-                       } else {
-                           $impares[$cuentaImpares] = $numero[$i];
-                           $cuentaImpares++;
-                       }
+  // Guarda los pares y los impares en arrays diferentes
+  $cuentaPares = 0;
+  $cuentaImpares = 0;
 
-                    // Guarda los datos en el array original
-                    // según lo que pide el ejercicio
+  for ($i = 0; $i < 20; $i++) {
+    if ($numero[$i] % 2 == 0) {
+      $pares[$cuentaPares] = $numero[$i];
+      $cuentaPares++;
+    } else {
+      $impares[$cuentaImpares] = $numero[$i];
+      $cuentaImpares++;
+    }
+  }
 
-                    for ($i = 0; $i < $cuentaPares; $i++)
-                       $numero[$i] = $pares[$i];
-                    
-                    for ($i = $cuentaPares; $i < $cuentaPares + $cuentaImpares; $i++)
-                       $numero[$i] = $impares[$i - $cuentaPares];
-            
-                    
-                    // Muestra el array resultante
+  // Guarda los datos en el array original
+  // según lo que pide el ejercicio
 
-                    // Índice
-                    echo "<br>Array resultante con los pares al principio y los impares al final:<br>";
-                    echo "<table><tr>";
-                    for ($i = 0; $i < 20; $i++)
-                       echo "<td>$i</td>";
-                    echo "</tr>";
-                    
-                    // Contenido
-                    for ($i = 0; $i < 20; $i++)
-                       echo "<td>".$numero[$i]."</td>";
-                    echo "</tr></table>";
-              
+  for ($i = 0; $i < $cuentaPares; $i++) {
+    $numero[$i] = $pares[$i];
+  }
+
+  for ($i = $cuentaPares; $i < $cuentaPares + $cuentaImpares; $i++) {
+    $numero[$i] = $impares[$i - $cuentaPares];
+  }
+
+  // Muestra el array resultante
+
+  // Índice
+  echo "<br>Array resultante con los pares al principio y los impares al final:<br>";
+  echo "<table><tr>";
+  for ($i = 0; $i < 20; $i++) {
+    echo "<td>$i</td>";
+  }
+  echo "</tr>";
+
+  // Contenido
+  for ($i = 0; $i < 20; $i++) {
+    echo "<td>".$numero[$i]."</td>";
+  }
+  echo "</tr></table>";
