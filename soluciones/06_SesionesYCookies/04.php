5d9f01d3d8b191b98fa4392d0f231aa065bfc62f
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-10 00:38:40 +0100

    Revisando las soluciones a los ejercicios del capítulo 6 - Sesiones y cookies

---
+++ soluciones/06_SesionesYCookies/04.php
@@ -0,0 +1,52 @@
+<?php
+	if (!isset($_SESSION['logueado'])) {
+		$_SESSION['logueado'] = false;
+  } 
+  
+  // Formulario de login
+  if (!$_SESSION['logueado']) {
+  ?>
+    <p>Debe iniciar sesión para acceder a la aplicación.</p>
+    <form action="pagina.php" method="post">
+      <input type="hidden" name="ejercicio" value="04">
+      Usuario: <input type="text" name="usuario" autofocus><br>
+      Contraseña: <input type="password" name="contrasena"><br><br>
+      <input type="submit" value="Iniciar sesión">
+    </form>
+    <br>
+  <?php
+  }
+
+  // Comprueba nombre de usuario y contraseña
+  if (isset($_POST['usuario'])) {
+    if (($_POST['usuario'] == "tux") && ($_POST['contrasena'] == "xut")) {
+      $_SESSION['logueado'] = true;
+      header("Refresh: 0; url=pagina.php?ejercicio=04", true, 303); // recarga la página
+    } else {
+      echo '<span style="color: red">Contraseña incorrecta. Inténtelo de nuevo.</span><br><br>';
+      header("Refresh: 3; url=pagina.php?ejercicio=04", true, 303); // recarga la página
+    }
+  }
+  
+  if ($_SESSION['logueado']) {
+    // Ejercicio 1 de la relación de Arrays /////////
+    for ($i = 0; $i < 20; $i++) {
+      $numero[] = rand(0,100);
+    }
+
+    foreach ($numero as $elemento) {
+      $cuadrado[] = $elemento * $elemento;
+      $cubo[] = $elemento * $elemento * $elemento;
+    }
+
+    echo "<table>";
+    echo "<tr><td>Número</td><td>Cuadrado</td><td>Cubo</td></tr>";
+    for ($i = 0; $i < 20; $i++) {
+      echo "<tr><td>".$numero[$i]."</td>";
+      echo "<td>".$cuadrado[$i]."</td>";
+      echo "<td>".$cubo[$i]."</td></tr>";
+    }
+    echo "</table>";
+    //////////////////////////////////////////////////
+  } 
+?>
---
+++ soluciones/06_SesionesYCookies/05.php
@@ -0,0 +1,69 @@
+<h3>Tienda on-line <b><i><u>La Estilográfica</u></i></b></h3>
+<table style="border: 2px; margin: 40px;"><tr><td>
+<h3>Productos</h3><hr>
+<?php // Tienda ///////////////////////////////////////////////////////
+
+  $producto = array ( 
+    "peli1000" => array( "nombre" => "Pelikan Souvëran M-1000", "precio" => 545, "imagen" => "pelikan.jpg"),
+    "parkduo" => array( "nombre" => "Parker Duofold International", "precio" => 406, "imagen" => "parkerduo.jpg"),
+    "viscvan" => array( "nombre" => "Visconti Van Gogh", "precio" => 180, "imagen" => "visconti.jpg"),
+    "waterexp" => array( "nombre" => "Waterman Expert", "precio" => 103.55, "imagen" => "waterman.jpg")
+  );
+
+  foreach ($producto as $codigo => $elemento) {
+    ?>
+    <img src="img/<?=$elemento[imagen]?>" width="360" border="1"><br>
+    <?=$elemento[nombre]?><br>Precio: <?=$elemento[precio]?> €
+    <form action="pagina.php" method="post">
+      <input type="hidden" name="ejercicio" value="05">
+      <input type="hidden" name="codigo" value="<?=$codigo?>">
+      <input type="hidden" name="accion" value="comprar">
+      <input type="submit" value="Comprar">
+    </form><br><br>
+    <?php
+  }					
+  ?>
+
+  </td><td>			
+  <h3>Carrito</h3><hr>
+
+  <?php // Carrito ///////////////////////////////////////////////////////
+  $accion = $_POST['accion'];
+  $codigo = $_POST['codigo'];
+
+  // Inicializa el carrito la primera vez
+  if (!isset($_SESSION[carrito])) {
+    $_SESSION[carrito] = array ("peli1000" => 0, "parkduo" => 0, "viscvan" => 0, "waterexp" => 0);
+  }
+
+  if ($accion == "comprar") {
+    $_SESSION[carrito][$codigo]++;
+  }
+
+  if ($accion == "eliminar") {
+    $_SESSION[carrito][$codigo] = 0;
+  }
+
+  // Muestra el contenido del carrito
+  $total = 0;
+  foreach ($producto as $cod => $elemento) {
+    if ($_SESSION[carrito][$cod] > 0) {
+      $total = $total + ($_SESSION[carrito][$cod] * $elemento[precio]);
+      ?>
+      <img src="img/<?=$elemento[imagen]?>" width="200" border="1"><br>
+      <?=$elemento[nombre]?><br>Precio: <?=$elemento[precio]?> €<br>
+      Unidades: <?=$_SESSION[carrito][$cod]?>
+      <form action="pagina.php" method="post">
+        <input type="hidden" name="ejercicio" value="05">
+        <input type="hidden" name="codigo" value="<?=$cod?>">
+        <input type="hidden" name="accion" value="eliminar">
+        <input type="submit" value="Eliminar">
+      </form><br><br>
+      <?php
+    }
+  }
+  ?>
+  <b>Total: <?=$total?> €</b>
+  </td>
+  </tr>
+</table>
