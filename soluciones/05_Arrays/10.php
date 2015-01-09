67e591226539d29e59be0668a197248316ac61b3
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-09 10:29:52 +0100

    Revisando ejercicios del capítulo 5 - Arrays

--- soluciones/05_Arrays/10.php
+++ soluciones/05_Arrays/10.php
@@ -1,25 +1,27 @@
+<?php
+    $puntuacion = array (
+      'as' => 11, 'dos' => 0, 'tres' => 10, 'cuatro' => 0, 'cinco' => 0,
+      'seis' => 0, 'siete' => 0, 'sota' => 2, 'caballo' => 3, 'rey' => 4);
+    
+    $palo = array ('oros', 'copas', 'bastos', 'espadas');
+    
+    $figura = array ('as', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'sota', 'caballo', 'rey');
+    
+    $cartasEchadas = "";
+    $contadorCartasEchadas = 0;
+    $puntosTotales = 0;
 
-                <?php
-                    $puntuacion = array ('as' => 11, 'dos' => 0, 'tres' => 10, 'cuatro' => 0, 'cinco' => 0, 'seis' => 0, 'siete' => 0, 'sota' => 2, 'caballo' => 3, 'rey' => 4);
-                    $palo = array ('oros', 'copas', 'bastos', 'espadas');
-                    $figura = array ('as', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'sota', 'caballo', 'rey');
-                    $cartasEchadas = "";
-                    $contadorCartasEchadas = 0;
-                    $puntosTotales = 0;
- 
-                    do {
+    do {
+      $paloCarta = $palo[rand(0, 3)];
+      $figuraCarta = $figura[rand(0, 9)];
+      $puntosCarta = $puntuacion[$figuraCarta];
+      $nombreCarta = "$figuraCarta de $paloCarta";
+      if (!in_array($nombreCarta, $cartasEchadas)) {
+        echo "$nombreCarta - $puntosCarta puntos<br>";
+        $cartasEchadas[] = $nombreCarta;
+        $contadorCartasEchadas++;
+        $puntosTotales += $puntosCarta;
+      }
+    } while ($contadorCartasEchadas < 10); 
 
-                        $paloCarta = $palo[rand(0, 3)];
-                        $figuraCarta = $figura[rand(0, 9)];
-                        $puntosCarta = $puntuacion[$figuraCarta];
-                        $nombreCarta = "$figuraCarta de $paloCarta";
-                        if (!in_array($nombreCarta, $cartasEchadas)) {
-                            echo "$nombreCarta - $puntosCarta puntos<br>";
-                            $cartasEchadas[] = $nombreCarta;
-                            $contadorCartasEchadas++;
-                            $puntosTotales += $puntosCarta;
-                        }       
-                    } while ($contadorCartasEchadas < 10); 
-                    
-                    echo "<br><b>Total: $puntosTotales puntos</b>";
-                ?>
+    echo "<br><b>Total: $puntosTotales puntos</b>";
