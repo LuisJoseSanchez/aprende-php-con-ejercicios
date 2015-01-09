235265b9cf181844a270f781fc03a4f09d211dd9
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-09 10:49:35 +0100

    Revisando ejercicios del capítulo 5 - Arrays

--- soluciones/05_Arrays/11.php
+++ soluciones/05_Arrays/11.php
@@ -1,45 +1,46 @@
+<h2>Diccionario español - inglés</h2>
+<?php
+  if (isset($_GET['palabra'])) {
+    $palabra = $_GET['palabra'];
 
-                <h2>Diccionario español - inglés</h2>
-                <?php
-                    if (isset($_POST['palabra'])) {
-                         $palabra = $_POST['palabra'];
-                   
-                         $diccionario = array (
-                            "ordenador" => "computer",
-                            "gato" => "cat",      
-                            "rojo" => "red",
-                            "árbol" => "tree",
-                            "pingüino" => "penguin",
-                            "sol" => "sun",
-                            "agua" => "water",
-                            "viento" => "wind",
-                            "siesta" => "siesta",
-                            "arriba" => "up",
-                            "ratón" => "mouse",
-                            "estadio" => "arena",
-                            "calumnia" => "aspersion",
-                            "aguacate" => "avocado",
-                            "cuerpo" => "body",
-                            "concurso" => "contest",
-                            "cena" => "dinner",
-                            "salida" => "exit",
-                            "lenteja" => "lentil",
-                            "cacerola" => "pan",
-                            "pastel" => "pie",
-                            "membrillo" => "quince"
-                            );
+    $diccionario = array (
+       "ordenador" => "computer",
+       "gato" => "cat",      
+       "rojo" => "red",
+       "árbol" => "tree",
+       "pingüino" => "penguin",
+       "sol" => "sun",
+       "agua" => "water",
+       "viento" => "wind",
+       "siesta" => "siesta",
+       "arriba" => "up",
+       "ratón" => "mouse",
+       "estadio" => "arena",
+       "calumnia" => "aspersion",
+       "aguacate" => "avocado",
+       "cuerpo" => "body",
+       "concurso" => "contest",
+       "cena" => "dinner",
+       "salida" => "exit",
+       "lenteja" => "lentil",
+       "cacerola" => "pan",
+       "pastel" => "pie",
+       "membrillo" => "quince"
+       );
 
-                         foreach ($diccionario as $clave => $valor)
-                            $palabrasEspanolas[] = $clave;
-                         
-                         if (in_array($palabra, $palabrasEspanolas))
-                                echo "<b>$palabra</b> en inglés es <b>".$diccionario[$palabra]."</b><br><br>";
-                            else
-                                echo "Lo siento, no conozco esa palabra.<br><br>";
-                    }
-                     
-                    echo '<form action="11.php" method="post">';
-                    echo 'Palabra en español :';
-                    echo '<input type="text" name ="palabra" autofocus>';
-                    echo '<input type="submit" value="OK">';
-                    echo '</form>';
+    foreach ($diccionario as $clave => $valor) {
+      $palabrasEspanolas[] = $clave;
+    }
+    
+    if (in_array($palabra, $palabrasEspanolas)) {
+      echo "<b>$palabra</b> en inglés es <b>".$diccionario[$palabra]."</b><br><br>";
+    } else {
+      echo "Lo siento, no conozco esa palabra.<br><br>";
+    }
+  }
+?>
+<form action="pagina.php" method="get">
+  <input type="hidden" name="ejercicio" value="11">
+  Palabra en español : <input type="text" name ="palabra" autofocus="" required="">
+  <input type="submit" value="OK">
+</form>
