e2b13ff99614ad0d5feefc73996361b65ea0d78e
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-09-23 11:12:37 +0200

    Actualizando ejemplos del capítulo 3: Sentencia condicional.

---
+++ ejemplos/03_SentenciaCondicional/condicionalAbreviado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $x = (20 > 6) ? 11 : 22;
+      echo $x;
+    ?>
+  </body>
+</html>
