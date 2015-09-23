41115d8e0a5a72796a1a7dc750daeb289ac6b2b4
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-09-23 09:58:06 +0200

    Actualizando ejemplos del capítulo 2: Recogida de datos por teclado mediante formularios.

---
+++ ejemplos/02_RecogidaDeDatosPorTeclado/suma/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h3>Calcula la suma de dos números</h3>
+    <form action="suma.php" method="get">
+    Primer número: <input type="number" name="a"><br>
+    Segundo número: <input type="number" name="b"><br>
+    <input type="submit" value="Sumar">
+    </form>
+  </body>
+</html>
---
+++ ejemplos/02_RecogidaDeDatosPorTeclado/suma/suma.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+      $a = $_GET['a'];
+      $b = $_GET['b'];
+      echo "La suma de $a mas $b es ", $a + $b;
+    ?>
+  </body>
+</html>
