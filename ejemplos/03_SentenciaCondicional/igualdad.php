dfffc447c04eea9f3858d9d3e905ec2ddf9ec00f
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-09-23 10:45:28 +0200

    Actualizando ejemplos del capítulo 3: Sentencia condicional.

---
+++ ejemplos/03_SentenciaCondicional/igualdad.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $a = 5;
+      $b = "5";
+      echo ($a == 5)?"verdadero":"falso", "<br>";
+      echo ($a === 5)?"verdadero":"falso", "<br>";
+      echo ($b == 5)?"verdadero":"falso", "<br>";
+      echo ($b === 5)?"verdadero":"falso", "<br>";
+    ?>
+  </body>
+</html>
