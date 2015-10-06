e5b139958fe55d0d9105541fd2b5dcaa71183f63
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-10-06 20:11:41 +0200

    Actualizando ejemplos del Capítulo 5: Arrays

---
+++ ejemplos/05_Arrays/arrayAsociativoConCorchetes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+      $estatura = [
+        "Rosa" => 168,
+        "Ignacio" => 175,
+        "Daniel" => 172,
+        "Rubén" => 182,
+      ];
+      echo "La estatura de Daniel es ", $estatura['Daniel'] , " cm"; 
+    ?>
+  </body>
+</html>
