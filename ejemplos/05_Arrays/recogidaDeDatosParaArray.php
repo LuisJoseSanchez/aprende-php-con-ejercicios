380a61e403ea54f394492dd34820bc8a6f437690
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-10-06 20:50:41 +0200

    Actualizando ejemplos del Capítulo 5: Arrays

--- ejemplos/05_Arrays/recogidaDeDatosParaArray.php
+++ ejemplos/05_Arrays/recogidaDeDatosParaArray.php
@@ -5,39 +5,34 @@
   </head>
   <body>
     <?php
-      if (!isset($_GET['n'])) {
-        $contadorNumeros = 1;
+      $n = $_GET['n'];
+      $contadorNumeros = $_GET['contadorNumeros'];
+      $numeroTexto = $_GET['numeroTexto'];
+    
+      if (!isset($n)) {
+        $contadorNumeros = 0;
         $numeroTexto = "";
-        echo '<form action="#" method="get">';
-        echo 'Introduzca un número:';
-        echo '<input type="text" name ="n" autofocus>';
-        echo '<input type="hidden" name="contadorNumeros" value="'.$contadorNumeros.'">';
-        echo '<input type="submit" value="OK">';
-        echo '</form>';
-      } else {
-        $contadorNumeros = $_GET['contadorNumeros'];
-        $n = $_GET['n'];
-        $numeroTexto = $_GET['numeroTexto'];
-
-        if ($numeroTexto=="") {
-          $numeroTexto = $n;
-        } else {
-          $numeroTexto = $numeroTexto.' '.$n;
-        }
-
-        if ($contadorNumeros < 4) {
-          $contadorNumeros++;
-          echo '<form action="#" method="get">';
-          echo 'Introduzca un número:';
-          echo '<input type="text" name="n" autofocus>';
-          echo '<input type="hidden" name="contadorNumeros" value="'.$contadorNumeros.'">';
-          echo '<input type="hidden" name="numeroTexto" value="'.$numeroTexto.'">';
-          echo '<input type="submit" value="OK">';
-          echo '</form>';
-        } else {
-          $numero = explode(" ", $numeroTexto);
-          echo "Los números introducidos son: ";
-          foreach ($numero as $n)
+      }
+      
+      // Pide número y añade el actual a la cadena
+      if (($contadorNumeros < 6) || (!isset($n))) {
+        ?>
+        <form action="#" method="get">
+          Introduzca un número:
+          <input type="number" name ="n" autofocus>
+          <input type="hidden" name="contadorNumeros" value="<?= ++$contadorNumeros ?>">
+          <input type="hidden" name="numeroTexto" value="<?= $numeroTexto . " " . $n ?>">
+          <input type="submit" value="OK">
+        </form>
+        <?php
+      }
+      
+      // Muestra los números introducidos
+      if ($contadorNumeros == 6) {
+        $numeroTexto = $numeroTexto . " " . $n; // añade el último número leído
+        $numero = explode(" ", $numeroTexto);
+        echo "Los números introducidos son: ";
+        foreach ($numero as $n) {
           echo $n, "  ";
         }
       }
