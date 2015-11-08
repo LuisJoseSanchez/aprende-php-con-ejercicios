9590116baf6076d39bde7cebf78adde590d54d4a
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-11-08 11:31:04 +0100

    Actualizando ejemplos de PDO del Capítulo 7: Acceso a Bases de Datos

---
+++ ejemplos/07_AccesoABasesDeDatos/PDO/listadopdo.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h2>
+      Base de datos <u>banco</u><br>
+      Tabla <u>cliente</u><br>
+    </h2>
+    <?php
+    
+      // Conexión a la base de datos
+      try {
+        // Se puede utilizar MySQL o PostgreSQL
+        $conexion = new PDO("mysql:host=localhost", "root", "root");
+        //$conexion = new PDO("posgresql:host=localhost", "root", "root");
+        echo "Se ha establecido una conexión con el servidor de bases de datos.";
+      } catch (PDOException $e) {
+        echo "No se ha podido establecer conexión con el servidor de bases de datos.<br>";
+        die ("Error: " . $e->getMessage());
+      }
+      $conexion = new mysqli("localhost", "root", "root");
+      if ($conexion->connect_errno > 0) {
+        echo "No se ha podido establecer conexión con el servidor de bases de datos.<br>";
+        die ("Error: " . $conexion->connect_error);
+      } else {
+        $conexion->select_db("banco");
+        $conexion->set_charset("utf8");
+
+        $consulta = $conexion->query("SELECT dni, nombre, direccion, telefono FROM cliente");
+          
+        ?>
+        <table border="1">
+        <tr>
+          <td><b>DNI</b></td>
+          <td><b>Nombre</b></td>
+          <td><b>Dirección</b></td>
+          <td><b>Teléfono</b></td>
+        </tr>
+
+        <?php
+        while ($cliente = $consulta->fetch_object()){
+          ?>
+          <tr>
+            <td><?= $cliente->dni ?></td>
+            <td><?= $cliente->nombre ?></td>
+            <td><?= $cliente->direccion ?></td>
+            <td><?= $cliente->telefono ?></td>
+          </tr>
+          <?php
+        }
+      }
+      $conexion->close();
+    ?>
+  </body>
+</html>
