9641582712566e0fa1a4b78d5c6ed3d30191bd15
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-01-06 20:30:30 +0100

    Actualizando ejemplos del Capítulo 10: Modelo Vista Controlador

--- ejemplos/10_Modelo_Vista_Controlador/OfertasPizzeriaBasico/Model/PizzeriaDB.php
+++ ejemplos/10_Modelo_Vista_Controlador/OfertasPizzeriaBasico/Model/PizzeriaDB.php
@@ -13,18 +13,7 @@ abstract class PizzeriaDB {
       echo "No se ha podido establecer conexión con el servidor de bases de datos.<br>";
       die ("Error: " . $e->getMessage());
     }
-    
-  /*  
-    $conexion = mysql_connect(self::$servidor, self::$usuario, self::$clave)
-      or die (mysql_error());
-    mysql_select_db(self::$base, $conexion);
-  */
-    
+ 
     return $connection;
   }
-  
-  /*
-  public static function desconectaDB() {
-    mysql_close();
-  }*/
 }
