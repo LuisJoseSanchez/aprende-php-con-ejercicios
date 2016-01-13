6d41d4981b6ba9aacd7f0c4401ee4a6fd96fced6
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-01-13 10:49:33 +0100

    Actualizando ejemplos del Capítulo 11: Twig - Motor de plantillas

--- ejemplos/11_Twig/DatosDeUsuarioTwig01/index.php
+++ ejemplos/11_Twig/DatosDeUsuarioTwig01/index.php
@@ -1,16 +1,14 @@
 <?php
   require_once 'twig/lib/Twig/Autoloader.php';
   Twig_Autoloader::register();
-  $loader = new Twig_Loader_Filesystem(__DIR__.'/plantillas');
+  $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
   $twig = new Twig_Environment($loader);
 
-  $infoUsuario = $twig->loadTemplate('infoUsuario.html.twig');
-
-  $datos = array(
+  $datos = [
     'nombre' => 'Elena',
     'apellido1' => 'Nito',
     'apellido2' => 'Del Bosque',
-    'dni' => '1234567X');
+    'dni' => '1234567X',
+  ];
 
-  $paginaUsuario = $infoUsuario->render($datos);
-  echo $paginaUsuario;
+  echo $twig->render('infoUsuario.html.twig', $datos);
