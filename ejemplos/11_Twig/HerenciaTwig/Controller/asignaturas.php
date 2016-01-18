8a25a4a8aa6f113017b65b211e695bb365bcef3a
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-01-18 13:32:19 +0100

    Actualizando ejemplos del Capítulo 11: Twig - Motor de plantillas

---
+++ ejemplos/11_Twig/HerenciaTwig/Controller/asignaturas.php
@@ -0,0 +1,7 @@
+<?php
+  require_once 'twig/lib/Twig/Autoloader.php';
+  Twig_Autoloader::register();
+  $loader = new Twig_Loader_Filesystem(__DIR__.'/../View');
+  $twig = new Twig_Environment($loader);
+
+  echo $twig->render('asignaturas.html.twig', []);
--- ejemplos/11_Twig/HerenciaTwig/index.php
+++ ejemplos/11_Twig/HerenciaTwig/index.php
@@ -1,13 +1,2 @@
 <?php
-  // Carga el entorno Twig
-  require_once 'twig/lib/Twig/Autoloader.php';
-  Twig_Autoloader::register();
-  $loader = new Twig_Loader_Filesystem(__DIR__.'/plantillas');
-  $twig = new Twig_Environment($loader);
-  //////////////////////////////////////////////////////////
-
-  $principal = $twig->loadTemplate('principal.html.twig');
-  // $paginaPrincipal = $principal->render(array());
-  // echo $paginaPrincipal;
-  $twig->loadTemplate('principal.html.twig')->display(array());
-?>
+  header("Location: Controller/index.php");
